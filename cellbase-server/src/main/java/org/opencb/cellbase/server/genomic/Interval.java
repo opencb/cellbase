f3e3021d65c03cdca6551e3b11b83fa3ff57b3b0
Francisco Salavert Torres <fsalavert@cipf.es>
2013-09-16 16:21:50 +0200

    server: war structure added; mongo core and server: created resources dir and moved .properties files

---
+++ cellbase-server/src/main/java/org/opencb/cellbase/server/genomic/Interval.java
@@ -0,0 +1,14 @@
+package org.opencb.cellbase.server.genomic;
+
+@Deprecated
+public class Interval {
+	public int start;
+	public int end;
+	public float value;
+	
+	public Interval(int start, int end, float value) {
+		this.start = start;
+		this.end = end;
+		this.value = value;
+	}
+}
