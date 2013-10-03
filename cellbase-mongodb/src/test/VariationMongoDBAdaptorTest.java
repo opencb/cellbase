79f1451e0e233ac821feb151a20416e569745c06
Francisco Salavert Torres <fsalavert@cipf.es>
2013-10-03 18:28:21 +0200

    PositionWSServer implemented snp by position web service GET and POST

---
+++ cellbase-mongodb/src/test/VariationMongoDBAdaptorTest.java
@@ -0,0 +1,34 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.BasicConfigurator;
+import org.junit.Test;
+import org.opencb.cellbase.core.common.Position;
+import org.opencb.cellbase.core.lib.DBAdaptorFactory;
+import org.opencb.cellbase.core.lib.api.variation.VariationDBAdaptor;
+import org.opencb.cellbase.core.lib.dbquery.QueryOptions;
+import org.opencb.cellbase.core.lib.dbquery.QueryResult;
+import org.opencb.cellbase.lib.mongodb.MongoDBAdaptorFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+public class VariationMongoDBAdaptorTest {
+    protected static DBAdaptorFactory dbAdaptorFactory;
+
+    static {
+        dbAdaptorFactory = new MongoDBAdaptorFactory();
+    }
+
+    @Test
+    public void testPosition() throws IOException {
+        System.out.println("works");
+
+        QueryOptions queryOptions = new QueryOptions();
+
+        List<Position> positionList = Position.parsePositions("13:32931667");
+        VariationDBAdaptor variationDBAdaptor = dbAdaptorFactory.getVariationDBAdaptor("hsapiens", "V3");
+
+        List<QueryResult> a = variationDBAdaptor.getAllByPositionList(positionList, queryOptions);
+        System.out.println(a);
+    }
+}
+
