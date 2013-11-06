ffab741bf7c4a9e23a6dddaf5ee1c61f58e98cdf
marnau <mariavall91@gmail.com>
2013-11-06 02:34:54 +0100

    web: panels and grids in different html files

--- cellbase-web/web/app/js/controllers/resultPanelController.js
+++ cellbase-web/web/app/js/controllers/resultPanelController.js
@@ -1,70 +1,299 @@
-//myApp.controller('resultPanelControl', ['$scope',function ($scope) {
-var resultPanelControl = myApp.controller('resultPanelController', ['$scope','mySharedService','Server', function ($scope, mySharedService, Server) {
+var resultPanelControl = myApp.controller('resultPanelController2', ['$scope','mySharedService','Server', function ($scope, mySharedService, Server) {
 
-//function resultPanelController($scope,mySharedService){
+    $scope.genesAndTranscriptsData=[];
+    $scope.genesData=[];
+    $scope.paginationData=[];
+
+    //------------------para el pagination--------------------
+    $scope.numeroDatosMostrar = 4;
+    //limites:
+    $scope.firstData = 0;
+    $scope.lastData = $scope.numeroDatosMostrar;
+    //--------------------------------------------------------
 
 
-    $scope.prueba=[1,2,3,4];
+    //obtener los datos que hay en el rango establecido por el paginado, es mas rapido que aplicar un filtro
+    $scope.getDataInPaginationLimits = function () {
 
+        $scope.paginationData = [];
 
-    $scope.species = ["Homo sapiens ","Mus musculus ","Rattus norvegicus ", "Danio rerio ","Drosophila melanogaster ","Caenorhabditis elegans ","Saccharomyces cerevisiae ","Canis familiaris ","Sus scrofa ","Anopheles gambiae ","Plasmodium falciparum"];
-//    $scope.species = ["hsapiens","mmusculus","rnorvegicus","drerio","dmelanogaster","celegans","scerevisiae","cfamiliaris","sscrofa","agambiae","pfalciparum"]
+        for (i=$scope.firstData; i<$scope.lastData; i++){
 
+            if($scope.genesAndTranscriptsData[i] != null){
+                 $scope.paginationData.push($scope.genesAndTranscriptsData[i]);
+            }
+        }
 
-    $scope.getSpecies = function () {
-        return mySharedService.species;
     };
 
-    $scope.selectedSpecie = "";
 
-    $scope.setSelectedSpecie = function (specie) {
-
-        $scope.selectedSpecie = specie;
-        console.log( $scope.selectedSpecie);
+    //indicara los datos que va a mostrar dependiendo del numero de la paginacion
+    $scope.setLimits = function (firstData) {
+        $scope.firstData = firstData;
+        $scope.lastData = firstData + $scope.numeroDatosMostrar;
     };
 
-    $scope.$on('handleBroadcast', function () {
-        $scope.message = mySharedService.message;
-        console.log("result:   " + $scope.message);
-    });
 
     $scope.$on('resultsBroadcast', function () {
 
-        var data = Server.get(mySharedService.selectedSpecie,mySharedService.selectedRegions);
-        console.log(data);
+        $scope.genesAndTranscriptsData = Server.getGenesAndTranscripts(mySharedService.selectedSpecies.shortName, mySharedService.selectedRegions);
+        $scope.genesData = Server.getGenes(mySharedService.selectedSpecies.shortName, mySharedService.selectedRegions);
+
+//        console.log($scope.genesAndTranscriptsData);
+//        console.log($scope.genesData);
+
+        //indicamos que los primeros datos a mostrar son los de la pagina 1
+        for (i=0; i<$scope.numeroDatosMostrar; i++){
+            if($scope.genesAndTranscriptsData[i] != null){
+                $scope.paginationData.push($scope.genesAndTranscriptsData[i]);
+            }
+        }
+
+
+        //definimos el pagination
+        var numeroDatos = $scope.genesAndTranscriptsData.length;  //21
+        var numeroDePaginas = Math.ceil(numeroDatos / $scope.numeroDatosMostrar);
+
+
+        var options = {
+            currentPage: 1,   //en la que nos encontramos inicialmente
+            totalPages: numeroDePaginas,
+            numberOfPages: 3,  //las que se ven en numero
+            size: 'mini',
+            onPageClicked: function(e,originalEvent,type,page){
+                $scope.setLimits((page-1) * $scope.numeroDatosMostrar);
+            }
+        }
+
+        $('#pagination').bootstrapPaginator(options);
+
 
     });
+
+
+
+    $scope.goToTab = function () {
+
+        $(function () {
+            $('#myTab a:first').tab('show')
+        })
+
+        $('#myTab a').click(function (e) {
+            e.preventDefault()
+            $(this).tab('show')
+        })
+
+
+    };
+
+//-------------------------------------------------
+    $scope.lastDataShow;
+
+    $scope.showGenesTable = false;
+
+    $scope.showAllGenes = function (index) {
+
+        $scope.showGenesTable = true;
+
+        $scope.showGenePanel = false;
+        $scope.showTranscriptPanel = false;
+        $scope.showTranscriptsTable = false;
+        $scope.showMoreInfoPanel = false;
+
+    };
+
+    $scope.showGenePanel = false;
+    $scope.selectedGen;
+
+    $scope.geneSelected = function (geneId) {
+
+
+         if($scope.lastDataShow != geneId){
+
+            $scope.lastDataShow = geneId;   //nuevo gen
+            $scope.showGenePanel = true;    //mostrar panel
+            $scope.selectedGen = Server.getGene(mySharedService.selectedSpecies.shortName, geneId).result[0];  //obtener los datos
+
+
+            $scope.showTranscriptPanel = false;
+            $scope.showTranscriptsTable = false;
+            $scope.showMoreInfoPanel = false;
+        }
+        else
+         {
+             if(!$scope.showGenePanel){  //para que no se muestre cuando ya lo esta
+                 $scope.showGenePanel = true;    //mostrar panel
+                 $scope.selectedGen = Server.getGene(mySharedService.selectedSpecies.shortName, geneId).result[0];  //obtener los datos
+             }
+         }
+            $scope.showGenesTable = false;
+    };
+
+
+    $scope.showTranscriptsTable = false;
+
+
+    $scope.transcriptsSelected = function (geneId) {
+
+        if($scope.lastDataShow != geneId){
+
+            $scope.lastDataShow = geneId;   //nuevo gen
+            $scope.showTranscriptsTable = true;
+            $scope.selectedTranscripts= Server.getGene(mySharedService.selectedSpecies.shortName, geneId).result[0].transcripts;
+
+            $scope.showTranscriptPanel = false;
+            $scope.showGenePanel = false;
+            $scope.showMoreInfoPanel = false;
+        }
+        else
+        {
+            if(!$scope.showTranscriptsTable){  //para que no se muestre cuando ya lo esta
+                $scope.showTranscriptsTable = true;
+                $scope.selectedTranscripts= Server.getGene(mySharedService.selectedSpecies.shortName, geneId).result[0].transcripts;
+            }
+        }
+        $scope.showGenesTable = false;
+
+
+    };
+
+
+    $scope.showTranscriptPanel = false;
+    $scope.selectedTranscript;
+
+    $scope.transcriptSelected = function (geneId, transcriptName) {
+
+        var transcripts;
+
+        if($scope.lastDataShow != geneId){
+            $scope.lastDataShow = geneId;   //nuevo gen
+
+            $scope.showTranscriptsTable = false;
+            $scope.showGenePanel = false;
+            $scope.showMoreInfoPanel = false;
+        }
+
+        $scope.showGenesTable = false;
+
+        $scope.showTranscriptPanel = true;
+        transcripts = Server.getGene(mySharedService.selectedSpecies.shortName, geneId).result[0].transcripts;
+
+        for(var i in transcripts)
+        {
+            if(transcripts[i].name == transcriptName ){
+                $scope.selectedTranscript = transcripts[i];
+            }
+        }
+
+    };
+
+
+    $scope.showMoreInfoPanel = false;
+    $scope.selectedExons;
+    $scope.selectedTFBS;
+    $scope.selectedXrefs;
+
+    $scope.moreInfoSelected = function (geneId, transcriptName) {
+
+        var transcripts;
+
+        if($scope.lastDataShow != geneId){
+
+            $scope.lastDataShow = geneId;   //nuevo gen
+
+            $scope.showTranscriptsTable = false;
+            $scope.showTranscriptPanel = false;
+            $scope.showGenePanel = false;
+
+        }
+        $scope.showGenesTable = false;
+
+            $scope.showMoreInfoPanel = true;
+            transcripts = Server.getGene(mySharedService.selectedSpecies.shortName, geneId).result[0].transcripts;
+
+            for(var i in transcripts)
+            {
+                if(transcripts[i].name == transcriptName ){
+
+                    $scope.selectedExons = transcripts[i].exons;
+                    $scope.selectedTFBS = transcripts[i].tfbs;
+                    $scope.selectedXrefs = transcripts[i].xrefs;
+
+                }
+            }
+    };
+
 //}
 }]);
 
 resultPanelControl.$inject = ['$scope','mySharedService'];
 
 
-
 myApp.factory('Server', function ($http) {
     return {
-        get: function(specie, regions) {
-
-            console.log(regions);
+        getGenesAndTranscripts: function(species, regions) {
 
             var dataGet;
-            var host = 'http://ws-beta.bioinfo.cipf.es/cellbasebeta2/rest/v3/'
+            var host = 'http://ws-beta.bioinfo.cipf.es/cellbase/rest/v3/'
 
             $.ajax({
-//                url: url,
-                url: host + specie + '/genomic/region/' + regions + '/gene?exclude=transcripts&of=json',
+                url: host + species + '/genomic/region/' + regions + '/gene?exclude=transcripts.xrefs,transcripts.exons,transcripts.tfbs&of=json',
                 async: false,
                 dataType: 'json',
                 success: function (data, textStatus, jqXHR) {
-                    dataGet=data[regions].result;
+
+                    dataGet=data.response[0].result;
                 },
                 error: function (jqXHR, textStatus, errorThrown) {
                 }
             });
 
             return dataGet;
-//            return $http.get(url);
+        },
+        getGenes: function(species, regions) {
 
+            var dataGet;
+            var host = 'http://ws-beta.bioinfo.cipf.es/cellbase/rest/v3/'
+
+            $.ajax({
+                url: host + species + '/genomic/region/' + regions + '/gene?exclude=transcripts&of=json',
+                async: false,
+                dataType: 'json',
+                success: function (data, textStatus, jqXHR) {
+
+                    dataGet=data.response[0].result;
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                }
+            });
+
+            return dataGet;
+        },
+        getGene: function(species, geneId) {
+
+            var dataGet;
+            var host = 'http://ws-beta.bioinfo.cipf.es/cellbase/rest/v3/'
+
+            $.ajax({
+                url: host + species + '/feature/gene/' + geneId + '/info?of=json',
+//              url: host + species + '/genomic/region/' + regions + '/gene?exclude=transcripts.xrefs,transcripts.exons,transcripts.tfbs&of=json',
+                async: false,
+                dataType: 'json',
+                success: function (data, textStatus, jqXHR) {
+
+                    dataGet = data.response[0];
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                }
+            });
+
+            return dataGet;
         }
     };
 });
+
+
+
+
+//----------tabs-----------------
+
