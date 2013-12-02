99927acf67abf308e514b3dfafe4e029cbd7d7f4
marnau <mariavall91@gmail.com>
2013-12-02 15:14:28 +0100

    making changes 0# Please enter the commit message for your changes. Lines starting

--- cellbase-web/web/vendor/bootstrap-paginator-master/build.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-
-uglifyjs ./src/bootstrap-paginator.js -o ./build/bootstrap-paginator.min.js -c sequences=true,dead_code=true,join_vars=true,evaluate=true,conditionals=true,loops=true,unused=false,if_return=true,cascade=true
