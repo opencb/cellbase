2b8d1b6d6e1bdddb2191200f01ed49c298e2c26d
imedina <imedina@cipf.es>
2013-12-26 19:31:09 +0100

    build: regulatory_region indexes fixed

--- cellbase-build/installation-dir/mongodb-scripts/regulation-indexes.js
+++ cellbase-build/installation-dir/mongodb-scripts/regulation-indexes.js
@@ -2,5 +2,5 @@
 db.getCollection('regulatory_region').ensureIndex({'chunkIds': 1})
 db.getCollection('regulatory_region').ensureIndex({'chromosome': 1, "start": 1, "end": 1})
 db.getCollection('regulatory_region').ensureIndex({'name': 1})
-db.getCollection('regulatory_region').ensureIndex({'featuresType': 1})
-db.getCollection('regulatory_region').ensureIndex({'featuresClass': 1})
+db.getCollection('regulatory_region').ensureIndex({'featureType': 1})
+db.getCollection('regulatory_region').ensureIndex({'featureClass': 1})
