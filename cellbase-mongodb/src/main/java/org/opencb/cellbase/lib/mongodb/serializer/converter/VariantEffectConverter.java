a54d8469cf48359f4f593ad14c71e45861dc385f
imedina <imedina@cipf.es>
2014-07-06 11:29:43 +0100

    mongodb: VariantAnnotation converter now also serializes frequencies, protein substitution scores and regulatory effects

--- cellbase-mongodb/src/main/java/org/opencb/cellbase/lib/mongodb/serializer/converter/VariantEffectConverter.java
+++ cellbase-mongodb/src/main/java/org/opencb/cellbase/lib/mongodb/serializer/converter/VariantEffectConverter.java
@@ -6,13 +6,10 @@ import com.google.common.collect.HashBiMap;
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
-import org.opencb.biodata.models.variant.effect.VariantAnnotation;
-import org.opencb.biodata.models.variant.effect.VariantEffect;
+import org.opencb.biodata.models.variant.effect.*;
 import org.opencb.cellbase.core.serializer.CellBaseTypeConverter;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by imedina on 17/06/14.
@@ -54,7 +51,8 @@ public class VariantEffectConverter implements CellBaseTypeConverter<VariantAnno
         Set<String> keys= variantAnnotation.getEffects().keySet();
         Iterator<String> iterator = keys.iterator();
 
-        // If a critical position 4 ALT alleles are pre-computed: one of A, C, G or T, and the '-'
+        // If the different allele at this genomic position can produce different results the 4 possible SNV ALT alleles
+        // are pre-computed: one of these A, C, G or T; and '-'
         // No more are possible can be found (in theory), but maybe some funny SNPs like AA/TTA exist so
         // if "size >= 4" then all possible values are stored.
         if(keys.size() >= 4) {
@@ -75,11 +73,11 @@ public class VariantEffectConverter implements CellBaseTypeConverter<VariantAnno
             }
         // If less than the 4 ALT alleles are found means that either 1 ALT allele and the '-' allele were pre-computed
         // (to minimize the number of calls to VEP), or a few multiallelic SNPs were found at that position.
-        // ALT allele must be stored as '*' allele when possible.
+        // ALT allele must be stored as '*' allele when possible to save space.
         }else {
             // If '-' is found then an allele independent position has been found and 1 allele and '-' are expected
             if(keys.contains("-")) {
-            System.out.println(keys+" "+variantAnnotation.getStart()+"-"+variantAnnotation.getEnd());
+                System.out.println(keys+" "+variantAnnotation.getStart()+"-"+variantAnnotation.getEnd());
                 boolean commonFound = false;
                 while(iterator.hasNext()) {
                     String key = iterator.next();
@@ -97,7 +95,7 @@ public class VariantEffectConverter implements CellBaseTypeConverter<VariantAnno
                         // Add Consequence Type object of this ALT to the List
                         consequenceTypeSchemaList.add(consequenceTypeDBObject);
                     }else {
-                        // Only the first non '-' is stored
+                        // Only the first non '-' is stored using '*'
                         if(!commonFound) {
                             commonFound = true;
                             List<VariantEffect> consequenceTypes = variantAnnotation.getEffects().get(key);
@@ -136,6 +134,7 @@ public class VariantEffectConverter implements CellBaseTypeConverter<VariantAnno
             // If no '-' is found we process all of them.
             else {
                 // 1077844
+                System.out.println(variantAnnotation.getStart());
                 while(iterator.hasNext()) {
                     String key = iterator.next();
                     List<VariantEffect> consequenceTypes = variantAnnotation.getEffects().get(key);
@@ -153,7 +152,31 @@ public class VariantEffectConverter implements CellBaseTypeConverter<VariantAnno
                 }
             }
         }
-        mongoDbSchema.append("ct", consequenceTypeSchemaList);
+        mongoDbSchema.append("eff", consequenceTypeSchemaList);
+
+        // Parsing Frequencies from VEP
+        BasicDBObject frequencyDBObject = parseFrequencies(variantAnnotation.getFrequencies());
+        if(frequencyDBObject.size() != 0) {
+            mongoDbSchema.append("freqs", frequencyDBObject);
+        }
+
+        // Parsing ProteinSubstitutionScores from VEP
+        BasicDBObject proteinSubstitutionScoresDBObject = parseProteinSubstituionScores(variantAnnotation.getProteinSubstitutionScores());
+        if(proteinSubstitutionScoresDBObject.size() != 0) {
+            mongoDbSchema.append("pss", proteinSubstitutionScoresDBObject);
+        }
+
+        // Parsing ProteinSubstitutionScores from VEP
+        BasicDBObject regulatoryEffectDBObject = parseRegulatoryEffect(variantAnnotation.getRegulatoryEffect());
+        if(regulatoryEffectDBObject.size() != 0) {
+            mongoDbSchema.append("reg", regulatoryEffectDBObject);
+        }
+
+        // Parsing ProteinSubstitutionScores from VEP
+//        BasicDBObject genesDBObject = parseGenes(variantAnnotation.getEffects());
+//        if(genesDBObject.size() != 0) {
+//            mongoDbSchema.append("gn", genesDBObject);
+//        }
 
         return mongoDbSchema;
     }
@@ -162,7 +185,6 @@ public class VariantEffectConverter implements CellBaseTypeConverter<VariantAnno
     public VariantAnnotation convertToDataModel(DBObject dbObject) {
         return null;
     }
-
 
 
 
@@ -203,7 +225,7 @@ public class VariantEffectConverter implements CellBaseTypeConverter<VariantAnno
         if(variantEffect.getConsequenceTypes() != null)
             consequenceTypeItemDBObject.append("ctTypes", variantEffect.getConsequenceTypes());
         if(variantEffect.isCanonical() != false)
-            consequenceTypeItemDBObject.append("isCcan", variantEffect.isCanonical());
+            consequenceTypeItemDBObject.append("isCan", variantEffect.isCanonical());
         if(variantEffect.getHgvsc() != null)
             consequenceTypeItemDBObject.append("hgvsc", variantEffect.getHgvsc());
         if(variantEffect.getHgvsp() != null)
@@ -223,5 +245,66 @@ public class VariantEffectConverter implements CellBaseTypeConverter<VariantAnno
     }
 
 
+    private BasicDBObject parseFrequencies(Frequencies frequencies) {
+        BasicDBObject frequencyItemDBObject = new BasicDBObject();
+        if(frequencies != null && frequencies.getAllele1000g() != null) {
+            frequencyItemDBObject.append("allele1000g", frequencies.getAllele1000g());
+            frequencyItemDBObject.append("maf1000G", frequencies.getMaf1000G());
+            frequencyItemDBObject.append("maf1000GAfrican", frequencies.getMaf1000GAfrican());
+            frequencyItemDBObject.append("maf1000GAmerican", frequencies.getMaf1000GAmerican());
+            frequencyItemDBObject.append("maf1000GAsian", frequencies.getMaf1000GAsian());
+            frequencyItemDBObject.append("maf1000GEuropean", frequencies.getMaf1000GEuropean());
+            frequencyItemDBObject.append("mafNhlbiEspAfricanAmerican", frequencies.getMafNhlbiEspAfricanAmerican());
+            frequencyItemDBObject.append("mafNhlbiEspEuropeanAmerican", frequencies.getMafNhlbiEspEuropeanAmerican());
+        }
+        return frequencyItemDBObject;
+    }
+
+    private BasicDBObject parseProteinSubstituionScores(ProteinSubstitutionScores proteinSubstitutionScores) {
+        BasicDBObject proteinSubstitionScoresItemDBObject = new BasicDBObject();
+        if(proteinSubstitutionScores.getPolyphenEffect() != null) {
+            proteinSubstitionScoresItemDBObject.append("ps", proteinSubstitutionScores.getPolyphenScore());
+            proteinSubstitionScoresItemDBObject.append("pe", proteinSubstitutionScores.getPolyphenEffect().name());
+
+        }
+        if(proteinSubstitutionScores.getSiftEffect() != null) {
+            proteinSubstitionScoresItemDBObject.append("ss", proteinSubstitutionScores.getSiftScore());
+            proteinSubstitionScoresItemDBObject.append("se", proteinSubstitutionScores.getSiftEffect().name());
+        }
+        return proteinSubstitionScoresItemDBObject;
+    }
+
+    private BasicDBObject parseRegulatoryEffect(RegulatoryEffect regulatoryEffect) {
+        BasicDBObject regulatoryItemDBObject = new BasicDBObject();
+        if(regulatoryEffect.getMotifName() != null) {
+            regulatoryItemDBObject.append("motifName", regulatoryEffect.getMotifName());
+            regulatoryItemDBObject.append("motifPosition", regulatoryEffect.getMotifPosition());
+            regulatoryItemDBObject.append("motifScoreChange", regulatoryEffect.getMotifScoreChange());
+            regulatoryItemDBObject.append("highInformationPosition", regulatoryEffect.isHighInformationPosition());
+            regulatoryItemDBObject.append("cellType", regulatoryEffect.getCellType());
+        }
+        return regulatoryItemDBObject;
+    }
+
+    private BasicDBObject parseGenes(Map<String, List<VariantEffect>> effects) {
+        BasicDBObject regulatoryItemDBObject = new BasicDBObject();
+
+        Set<String> visited = new HashSet<>();
+        Set<String> keys = effects.keySet();
+        Iterator<String> iterator = keys.iterator();
+        while(iterator.hasNext()) {
+            String next = iterator.next();
+            if(!visited.contains(next)) {
+
+
+
+                visited.add(next);
+            }
+        }
+
+
+//        regulatoryItemDBObject.append("cellType", variantEffect.ge);
+        return regulatoryItemDBObject;
+    }
 
 }
