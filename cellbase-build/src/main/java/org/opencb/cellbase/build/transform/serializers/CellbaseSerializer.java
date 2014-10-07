ff79c0636db77bb65c91aa07ae26c0f978bea708
Pablo Arce <pablo.arce@bioinfomgp.org>
2014-10-07 13:08:55 +0200

    Added untracked files

---
+++ cellbase-build/src/main/java/org/opencb/cellbase/build/transform/serializers/CellbaseSerializer.java
@@ -0,0 +1,34 @@
+package org.opencb.cellbase.build.transform.serializers;
+
+import org.opencb.cellbase.build.transform.MutationParser;
+import org.opencb.cellbase.build.transform.RegulatoryParser;
+import org.opencb.cellbase.core.common.GenericFeatureChunk;
+import org.opencb.cellbase.core.common.core.Gene;
+import org.opencb.cellbase.core.common.core.GenomeSequenceChunk;
+import org.opencb.cellbase.core.common.variation.Mutation;
+import org.opencb.cellbase.core.common.variation.Variation;
+
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: imedina
+ * Date: 8/28/13
+ * Time: 5:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface CellbaseSerializer {
+
+    public void serialize(Gene gene);
+
+    public void serialize(Variation variation);
+
+    public void serialize(GenericFeatureChunk genericFeatureChunk);
+
+    public void serialize(GenomeSequenceChunk genomeSequenceChunk);
+
+    public void serialize(Mutation mutation);
+
+    public void close();
+
+}
