89ef3c383cfe4bdb0a36943cd488cb1a2679adcf
Pablo Arce <pablo.arce@bioinfomgp.org>
2014-10-17 13:22:20 +0200

    GwasParser now is child of CellBaseParser

--- cellbase-build/src/test/groovy/org/opencb/cellbase/build/transform/GwasParserTest.groovy
+++ cellbase-build/src/test/groovy/org/opencb/cellbase/build/transform/GwasParserTest.groovy
@@ -1,6 +1,6 @@
 package org.opencb.cellbase.build.transform
 
-import org.opencb.cellbase.core.serializer.CellBaseSerializer
+import org.opencb.cellbase.build.serializers.CellBaseSerializer
 import spock.lang.Specification
 
 import java.nio.file.Paths
@@ -10,18 +10,18 @@ import java.nio.file.Paths
  */
 class GwasParserTest extends Specification {
 
-    def "parseFile"() {
+    def "parse"() {
         given: "a gwas file containing 18 lines, and a dbsnp subset containing all the snps of the gwas file"
         def serializer = Mock(CellBaseSerializer)
-        def gwasParser = new GwasParser(serializer)
         def gwasTestFile = Paths.get(VariantEffectParserTest.class.getResource("/gwasTest.csv").toURI())
         def dbSnpFile = Paths.get(VariantEffectParserTest.class.getResource("/dbSnpTest.gz").toURI())
+        def gwasParser = new GwasParser(serializer, gwasTestFile, dbSnpFile)
 
         when: "parse the gwas file"
-        gwasParser.parseFile(gwasTestFile, dbSnpFile)
+        gwasParser.parse()
 
         then: "11 variants should be created, because some of the lines of the input file are different studies or tests of the same variant"
-        11 * serializer.serializeObject(_)
+        11 * serializer.serialize(_)
     }
 
 }
