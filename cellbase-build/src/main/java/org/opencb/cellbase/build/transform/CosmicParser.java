5541bb632043a405f7e09d24503e89df44a29419
Pablo Arce <pablo.arce@bioinfomgp.org>
2014-10-22 10:16:23 +0200

    Refactorized CosmicParser, added TODOs

--- cellbase-build/src/main/java/org/opencb/cellbase/build/transform/CosmicParser.java
+++ cellbase-build/src/main/java/org/opencb/cellbase/build/transform/CosmicParser.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -62,28 +61,31 @@ public class CosmicParser extends CellBaseParser {
         // 26 Comments
 
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(cosmicFilePath.toFile())));
+            BufferedReader cosmicReader = new BufferedReader(new InputStreamReader(new FileInputStream(cosmicFilePath.toFile())));
             String line;
 
-            reader.readLine(); // First line is the header -> ignore it
+            cosmicReader.readLine(); // First line is the header -> ignore it
 
-            List<Cosmic> myList = new ArrayList<>(); // All Cosmic objects will be stored in a list
+            // TODO: remove the list, serialize created variants
+            List<Cosmic> cosmicList = new ArrayList<>();
 
-            while ((line = reader.readLine()) != null) {
+            while ((line = cosmicReader.readLine()) != null) {
                 String[] fields = line.split("\t", 27);
 
                 // For each variant contained, check out the sign of the strand
                 if (checkValidVariant(fields[19], fields[13], 1)){ // This function filters complex changes
                     // Create new COSMIC object
-                    myList.add(new Cosmic(fields));
+                    cosmicList.add(new Cosmic(fields));
                 }
             }
 
             // Sort objects by chromosome and position
-            Collections.sort(myList); // Sort function extends Comparable (which has been overrided in Cosmic.java class)
+            // TODO: sort here?
+            //Collections.sort(cosmicList); // Sort function extends Comparable (which has been overrided in Cosmic.java class)
 
             // Move through the ordered list and save each variant grouped by chromosome (a file for a given chromosome)
-            for (Cosmic cosmicVariant : myList) {
+            for (Cosmic cosmicVariant : cosmicList) {
+                // TODO: not efficient. Transform the chromosome when creating each cosmic to avoid browse the list
                 // In COSMIC database, X is referred as chr 23, Y as chr 24 and MT as chr 25. This is useful when sorting objects (see above).
                 // However, when printing to JSON, each object is transformed to that 23 becomes X, 24 becomes Y and 25 becomes MT
                 if (cosmicVariant.getChromosome().equals("23")){
@@ -96,6 +98,7 @@ public class CosmicParser extends CellBaseParser {
                     cosmicVariant.setChromosome("MT");
                     calculateMutation_NCBI36(cosmicVariant);
                 }
+
 
                 // serialize cosmic object
                 serialize(cosmicVariant);
@@ -119,47 +122,67 @@ public class CosmicParser extends CellBaseParser {
         if (genomePosition.equals(("")) || mutation_CDS.contains("?")) {
             validVariant = false;
         } else if(mutation_CDS.contains(">")) {
-            // Avoid changes of type c.8668CC>G, c.8668CC>GG, c.8668CC>GGG, c.8668CSSSSSC>G, etc
-            String ref = "";
-            String alt = mutation_CDS.split(">")[1];
-            String refAux = mutation_CDS.split(">")[0];
-            Matcher matcher = Pattern.compile("((A|C|G|T)+)").matcher(refAux);
-
-            if(matcher.find()) {// Either change or deletion
-                ref = matcher.group(); // Get the first group (entire pattern -> group() is equivalente to group(0)
-            }
-            if(ref.isEmpty() || alt.isEmpty() || ref.length() > 1 || alt.length() > 1) {
-                // Avoid variants with more than a single change (in either ref or alt)
-                validVariant = false; // for example,c.8668CC>G
-            }
+            validVariant = checkValidSustitution(mutation_CDS);
         } else if(mutation_CDS.contains("del")) {
-            // For deletions, only deletions of, at most, deletionLength nucleotide are allowed
-            if(mutation_CDS.split("del").length < 2) { // c.503_508del (usually, deletions of several nucleotides)
-                validVariant = false;
-            } else if(mutation_CDS.split("del")[1].matches("\\d+")) { //  c.503_508del30
-                validVariant = false;
-            } else if(mutation_CDS.split("del")[1].length() > deletionLength) {// c.503_508delCCT and deletionLength=1 (for example)
-                validVariant = false;
-            }
-        } else if(mutation_CDS.contains("ins") && mutation_CDS.split("ins")[1].matches("\\d+")) {
-            //c.503_508ins30
-            validVariant=false;
+            validVariant = checkValidDeletion(mutation_CDS, deletionLength);
+        } else if(mutation_CDS.contains("ins")) {
+            validVariant = checkValidInsertion(mutation_CDS.split("ins")[1]);
         } else if(mutation_CDS.contains("dup") || mutation_CDS.contains("Intronic")) {
-            validVariant=false;
+            // TODO: transform duplication
+            validVariant = false;
         }
 
         return validVariant;
     }
 
+    private boolean checkValidInsertion(String ins) {
+        boolean validVariant = true;
+        if (ins.matches("\\d+")) {
+            //c.503_508ins30
+            validVariant = false;
+        }
+        return validVariant;
+    }
+
+    private boolean checkValidDeletion(String mutation_CDS, int deletionLength) {
+        boolean validVariant = true;
+        // TODO: split is being done multiple times (not efficient)
+        // For deletions, only deletions of, at most, deletionLength nucleotide are allowed
+        if(mutation_CDS.split("del").length < 2) { // c.503_508del (usually, deletions of several nucleotides)
+            validVariant = false;
+        } else if(mutation_CDS.split("del")[1].matches("\\d+")) { //  c.503_508del30
+            validVariant = false;
+        } else if(mutation_CDS.split("del")[1].length() > deletionLength) {// c.503_508delCCT and deletionLength=1 (for example)
+            validVariant = false;
+        }
+        return validVariant;
+    }
+
+    private boolean checkValidSustitution(String mutation_CDS) {
+        boolean validVariant = true;
+        // Avoid changes of type c.8668CC>G, c.8668CC>GG, c.8668CC>GGG, c.8668CSSSSSC>G, etc
+        String ref = null;
+        String alt = mutation_CDS.split(">")[1];
+        String refAux = mutation_CDS.split(">")[0];
+        // TODO: pattern should be compiled once
+        Matcher matcher = Pattern.compile("((A|C|G|T)+)").matcher(refAux);
+
+        if(matcher.find()) {// Either change or deletion
+            ref = matcher.group(); // Get the first group (entire pattern -> group() is equivalente to group(0)
+        }
+        if(ref == null || ref.isEmpty() || alt.isEmpty() || ref.length() > 1 || alt.length() > 1) {
+            // Avoid variants with more than a single change (in either ref or alt)
+            validVariant = false; // for example,c.8668CC>G
+        }
+        return validVariant;
+    }
+
     private void calculateMutation_NCBI36(Cosmic cosmicVariant){
-        String genomePosition="";
-        String newGenomePosition="";
-
         // GRCh37 position
-        genomePosition = cosmicVariant.getMutation_GRCh37_genome_position();
-        newGenomePosition = cosmicVariant.getChromosome() + ":" + genomePosition.split(":")[1];
+        String genomePosition = cosmicVariant.getMutation_GRCh37_genome_position();
+        String newGenomePosition = cosmicVariant.getChromosome() + ":" + genomePosition.split(":")[1];
         cosmicVariant.setMutation_GRCh37_genome_position(newGenomePosition);
-
+        // TODO: NCBI 36 == HG37??
         // NCBI position
         genomePosition = cosmicVariant.getMutation_NCBI36_genome_position();
         if (!genomePosition.isEmpty()) {
