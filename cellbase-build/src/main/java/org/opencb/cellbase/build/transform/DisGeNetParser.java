15c87faf1ed78c3fb5bceb607ae2af4d814aa6fd
Pablo Arce <pablo.arce@bioinfomgp.org>
2014-10-24 08:54:08 +0200

    Fixed disgenet parser, now disgenet can store a list of ensembl gene ids

--- cellbase-build/src/main/java/org/opencb/cellbase/build/transform/DisGeNetParser.java
+++ cellbase-build/src/main/java/org/opencb/cellbase/build/transform/DisGeNetParser.java
@@ -1,13 +1,9 @@
 package org.opencb.cellbase.build.transform;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.opencb.biodata.models.feature.DisGeNet;
 import org.opencb.cellbase.build.serializers.CellBaseSerializer;
-;
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.nio.file.Path;
 import java.util.*;
 
@@ -16,50 +12,54 @@ import java.util.*;
  */
 public class DisGeNetParser  extends CellBaseParser {
     public Path disGeNetFilePath;
-    private Path EntrezIdToEnsemblIdFile;
+    private Path entrezIdToEnsemblIdFile;
 
 
-    public DisGeNetParser(CellBaseSerializer serializer, Path disGeNetFilePath, Path EntrezIdToEnsemblIdFile) {
+    public DisGeNetParser(CellBaseSerializer serializer, Path disGeNetFilePath, Path entrezIdToEnsemblIdFile) {
         super(serializer);
         this.disGeNetFilePath = disGeNetFilePath;
-        this.EntrezIdToEnsemblIdFile = EntrezIdToEnsemblIdFile;
+        this.entrezIdToEnsemblIdFile = entrezIdToEnsemblIdFile;
     }
 
 
-    private Map<String, String> parserEntrezIdToEnsemblIdFile(Path EntrezIdToEnsemblIdFile) {
-        Map<String, String> entrezIdToEnsemblId = new HashMap<>();
+    private Map<String, List<String>> parseEntrezIdToEnsemblIdFile(Path entrezIdToEnsemblIdFile) {
+        Map<String, List<String>> entrezIdToEnsemblId = new HashMap<>();
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(EntrezIdToEnsemblIdFile.toFile())));
-            String line;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(entrezIdToEnsemblIdFile.toFile())));
             reader.readLine(); // First line is the header -> ignore it
-            while ((line = reader.readLine()) != null) {
+            for (String line; (line = reader.readLine()) != null;) {
                 String[] fields = line.split("\t");
-                entrezIdToEnsemblId.put(fields[1], fields[0]);
+                List<String> ensemblIds = entrezIdToEnsemblId.get(fields[1]);
+                if (ensemblIds == null) {
+                    ensemblIds = new ArrayList<>();
+                    ensemblIds.add(fields[0]);
+                } else {
+                    ensemblIds.add(fields[0]);
+                }
+                entrezIdToEnsemblId.put(fields[1], ensemblIds);
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        // TODO: treat exceptions
         return entrezIdToEnsemblId;
     }
 
-
     public void parse() {
-        Map<String, String> entrezIdToEnsemblIdMap = this.parserEntrezIdToEnsemblIdFile(this.EntrezIdToEnsemblIdFile);
+        Map<String, List<String>> entrezIdToEnsemblIdMap = this.parseEntrezIdToEnsemblIdFile(this.entrezIdToEnsemblIdFile);
         Map<String,DisGeNet> disGeNetMap = new HashMap<>();
 
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(this.disGeNetFilePath.toFile())));
             reader.readLine(); // First line is the header -> ignore it
 
-
-            FillDisGeNetMap(entrezIdToEnsemblIdMap, disGeNetMap, reader);
+            fillDisGeNetMap(entrezIdToEnsemblIdMap, disGeNetMap, reader);
 
             Collection <DisGeNet> allDisGeNetRecords = disGeNetMap.values();
             for (DisGeNet one_disgenet : allDisGeNetRecords){
                 serialize(one_disgenet);
-
             }
 
         } catch (FileNotFoundException e) {
@@ -67,18 +67,15 @@ public class DisGeNetParser  extends CellBaseParser {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
-
     }
 
-    private void FillDisGeNetMap(Map<String, String> entrezIdToEnsemblIdMap, Map<String, DisGeNet> disGeNetMap, BufferedReader reader) throws IOException {
+    private void fillDisGeNetMap(Map<String, List<String>> entrezIdToEnsemblIdMap, Map<String, DisGeNet> disGeNetMap, BufferedReader reader) throws IOException {
         String line;
         while ((line = reader.readLine()) != null){
             String[] fields = line.split("\t");
 
-            String geneId = entrezIdToEnsemblIdMap.get(fields[0]);
+            String geneId = fields[0];
+            List<String> ensemblGeneIds = entrezIdToEnsemblIdMap.get(geneId);
             String geneSymbol = fields[1];
             String geneName = fields[2];
             String diseaseId = fields[3];
@@ -90,44 +87,39 @@ public class DisGeNetParser  extends CellBaseParser {
 
             if (geneId != null && geneId !=""){
                 if (disGeNetMap.get(geneId) != null){
-                    UpdateElementDisGeNetMap(disGeNetMap, geneId, diseaseId, diseaseName, score, numberOfPubmeds, associationType, sources);
-
+                    updateElementDisGeNetMap(disGeNetMap, geneId, ensemblGeneIds, diseaseId, diseaseName, score, numberOfPubmeds, associationType, sources);
+                } else {
+                    insertNewElementToDisGeNetMap(disGeNetMap, geneId, ensemblGeneIds, geneSymbol, geneName, diseaseId, diseaseName, score, numberOfPubmeds, associationType, sources);
                 }
-                else {
-                    InsertNewElementToDisGeNetMap(disGeNetMap, geneId, geneSymbol, geneName, diseaseId, diseaseName, score, numberOfPubmeds, associationType, sources);
-
-                }
-
             }
-
         }
     }
 
-    private void InsertNewElementToDisGeNetMap(Map<String, DisGeNet> disGeNetMap, String geneId, String geneSymbol, String geneName, String diseaseId, String diseaseName, Float score, Integer numberOfPubmeds, String associationType, Set<String> sources) {
-        DisGeNet.Disease diseaseToAddToNewGene = new DisGeNet.Disease (diseaseId,diseaseName,score,numberOfPubmeds,associationType,sources);
-        List<DisGeNet.Disease> listofDisease =  new ArrayList<>();
-        listofDisease.add(diseaseToAddToNewGene);
-        DisGeNet disGeNet = new DisGeNet(geneId,geneName,geneSymbol,listofDisease);
-        disGeNetMap.put(geneId,disGeNet);
+    private void insertNewElementToDisGeNetMap(Map<String, DisGeNet> disGeNetMap, String geneId, List<String> ensemblGeneIds, String geneSymbol, String geneName, String diseaseId, String diseaseName, Float score, Integer numberOfPubmeds, String associationType, Set<String> sources) {
+        DisGeNet.Disease diseaseToAddToNewGene =
+                new DisGeNet.Disease(diseaseId, diseaseName, score, numberOfPubmeds, associationType, sources);
+        List<DisGeNet.Disease> diseases = new ArrayList<>();
+        diseases.add(diseaseToAddToNewGene);
+        DisGeNet disGeNet = new DisGeNet(ensemblGeneIds, geneName, geneSymbol, diseases);
+        disGeNetMap.put(geneId, disGeNet);
     }
 
-    private void UpdateElementDisGeNetMap(Map<String, DisGeNet> disGeNetMap, String geneId, String diseaseId, String diseaseName, Float score, Integer numberOfPubmeds, String associationType, Set<String> sources) {
+    private void updateElementDisGeNetMap(Map<String, DisGeNet> disGeNetMap, String geneId, List<String> ensemblGeneIds, String diseaseId, String diseaseName, Float score, Integer numberOfPubmeds, String associationType, Set<String> sources) {
         DisGeNet disGeNetRecord = disGeNetMap.get(geneId);
         Boolean disease_found = false;
         for (int i = 0; i < disGeNetRecord.getDiseases().size(); i++){
             if (disGeNetRecord.getDiseases().get(i).getDiseaseId().equals(diseaseId)){
                 disGeNetRecord.getDiseases().get(i).getAssociationTypes().add(associationType);
                 disGeNetRecord.getDiseases().get(i).getSources().addAll(sources);
-                disGeNetMap.put(geneId,disGeNetRecord);
-
-                disease_found=true;
+                disGeNetMap.put(geneId, disGeNetRecord);
+                disease_found = true;
             }
-        if (disease_found == false) {
-            DisGeNet.Disease diseaseToAddToExitsGene = new DisGeNet.Disease (diseaseId,diseaseName,score,numberOfPubmeds,associationType,sources);
-            disGeNetRecord.getDiseases().add(diseaseToAddToExitsGene);
-
-            }
-
         }
+        if (disease_found == false) {
+            DisGeNet.Disease diseaseToAddToExitsGene =
+                    new DisGeNet.Disease(diseaseId, diseaseName, score, numberOfPubmeds, associationType, sources);
+            disGeNetRecord.getDiseases().add(diseaseToAddToExitsGene);
+        }
+        // TODO: no need to put the object again in the map?
     }
 }
