6c91c7cc13cfa099db3533d671952391e3200137
Pablo Arce <pablo.arce@bioinfomgp.org>
2014-12-02 14:01:47 +0100

    Fixed import

--- cellbase-build/src/main/java/org/opencb/cellbase/build/transform/ClinVarParser.java
+++ cellbase-build/src/main/java/org/opencb/cellbase/build/transform/ClinVarParser.java
@@ -1,6 +1,6 @@
 package org.opencb.cellbase.build.transform;
 
-import org.opencb.biodata.formats.feature.RefseqAccession;
+import org.opencb.biodata.formats.feature.refseq.RefseqAccession;
 import org.opencb.biodata.formats.variant.clinvar.ClinvarParser;
 import org.opencb.cellbase.build.transform.formats.clinical.ClinvarPublicSet;
 import org.opencb.biodata.formats.variant.clinvar.v19jaxb.MeasureSetType;
