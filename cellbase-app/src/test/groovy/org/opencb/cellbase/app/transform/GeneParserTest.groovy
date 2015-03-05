fb7db63b374cce7bfe1d67a528f99a60b3aedeac
Pablo Arce <pablo.arce@bioinfomgp.org>
2015-03-05 14:07:59 +0100

    New gene parser test

---
+++ cellbase-app/src/test/groovy/org/opencb/cellbase/app/transform/GeneParserTest.groovy
@@ -0,0 +1,34 @@
+package org.opencb.cellbase.app.transform
+
+import org.opencb.biodata.models.core.Gene
+import org.opencb.cellbase.app.serializers.CellBaseSerializer
+import spock.lang.Specification
+
+import java.nio.file.Paths
+
+/**
+ * Created by parce on 5/03/15.
+ */
+class GeneParserSpockTest extends Specification {
+
+
+    static List<Gene> serializedGenes
+
+    def setupSpec() {
+        def geneTestDir = Paths.get(GeneParserTest.class.getResource("/geneParser").toURI())
+        def genomeSequenceFasta = Paths.get(GeneParserTest.class.getResource("/geneParser/Homo_sapiens.GRCh38.fa").toURI())
+
+        // custom test serializer that adds the serialized variants to a list
+        def serializer = Mock(CellBaseSerializer)
+        serializedGenes = new ArrayList<Gene>()
+        serializer.serialize(_) >> { Gene arg -> serializedGenes.add(arg) }
+
+        def geneParser = new GeneParser(geneTestDir, genomeSequenceFasta, serializer)
+        geneParser.parse()
+    }
+
+
+    def "Parse"() {
+
+    }
+}
