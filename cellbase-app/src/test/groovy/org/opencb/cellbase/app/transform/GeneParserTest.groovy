cf61fca21a1cba17933afc9f77b082303c1816c3
Pablo Arce <pablo.arce@bioinfomgp.org>
2015-03-16 15:38:44 +0100

    GeneParserTest: added tests

--- cellbase-app/src/test/groovy/org/opencb/cellbase/app/transform/GeneParserTest.groovy
+++ cellbase-app/src/test/groovy/org/opencb/cellbase/app/transform/GeneParserTest.groovy
@@ -30,16 +30,37 @@ class GeneParserSpockTest extends Specification {
     }
 
     @Unroll
-    def "gene #geneId parsed has #transcriptsNumber transcripts"() {
+    def "gene #geneId has #transcriptsNumber transcripts"() {
         expect:
         serializedGenes.findAll({gene -> gene.getId().equals(geneId)}).size() == 1
         serializedGenes.findAll({gene -> gene.getId().equals(geneId)}).first().transcripts.size() == transcriptsNumber
 
         where:
         geneId || transcriptsNumber
-        "ENSG00000243485" | 2
-        "ENSG00000218839" | 2
+        "ENSG00000243485" || 2
+        "ENSG00000218839" || 2
     }
+
+    @Unroll
+    def "transcript #transcriptId from gene #geneId well parsed"() {
+        expect:
+        def transcript = serializedGenes.find({gene -> gene.getId() == geneId}).transcripts.find({transcript -> transcript.getId() == transcriptId})
+        transcript.getName() == name
+        transcript.getBiotype() == biotype
+        transcript.getStatus() == status
+        transcript.getChromosome() == chromosome
+        transcript.getStart() == start
+        transcript.getEnd() == end
+        transcript.getExons().size() == exonsNumber
+
+        where:
+        transcriptId     || geneId            | name             | biotype   | status  | chromosome | start | end   | strand | exonsNumber
+        "ENST00000473358"|| "ENSG00000243485" | "MIR1302-11-001" | "lincRNA" | "KNOWN" | "1"        | 29554 | 31097 | "+"    | 3
+        "ENST00000469289"|| "ENSG00000243485" | "MIR1302-11-002" | "lincRNA" | "KNOWN" | "1"        | 30267 | 31109 | "+"    | 2
+        "ENST00000449442"|| "ENSG00000218839" | "FAM138C-001"    | "lincRNA" | "KNOWN" | "9"        | 34394 | 35860 | "-"    | 3
+        "ENST00000305248"|| "ENSG00000218839" | "FAM138C-002"    | "lincRNA" | "KNOWN" | "9"        | 34965 | 35871 | "-"    | 2
+    }
+
 
     def cleanupSpec() {
         File referenceGenomeSqlLiteFile = Paths.get(GeneParserTest.class.getResource("/geneParser/reference_genome.db").toURI()).toFile()
