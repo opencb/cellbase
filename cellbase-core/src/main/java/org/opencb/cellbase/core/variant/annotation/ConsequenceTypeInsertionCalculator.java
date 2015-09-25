a61bbec1a1241e21bdb009e5bcacf77449ef452e
Javi Lopez <fjlopez@ebi.ac.uk>
2015-09-25 13:16:20 +0100

    feature/refactor-var-annot: small bug fixed at ConsequenceType calculators

--- cellbase-core/src/main/java/org/opencb/cellbase/core/variant/annotation/ConsequenceTypeInsertionCalculator.java
+++ cellbase-core/src/main/java/org/opencb/cellbase/core/variant/annotation/ConsequenceTypeInsertionCalculator.java
@@ -149,7 +149,8 @@ public class ConsequenceTypeInsertionCalculator extends ConsequenceTypeCalculato
             }
         }
 
-        if (consequenceTypeList.size() == 0 && isIntegernic) {
+//        if (consequenceTypeList.size() == 0 && isIntegernic) {
+        if (isIntegernic) {
             consequenceTypeList.add(new ConsequenceType(VariantAnnotationUtils.INTERGENIC_VARIANT));
         }
 
--- cellbase-mongodb/src/test/java/org/opencb/cellbase/mongodb/db/VariantAnnotationMongoDBAdaptorTest.java
+++ cellbase-mongodb/src/test/java/org/opencb/cellbase/mongodb/db/VariantAnnotationMongoDBAdaptorTest.java
@@ -285,9 +285,10 @@ public class VariantAnnotationMongoDBAdaptorTest {
         // TODO: check differences against Web VEP
 //        http://wwwdev.ebi.ac.uk/cellbase/webservices/rest/v3/hsapiens/genomic/variant/2:114340663:GCTGGGCATCC:ACTGGGCATCC/full_annotation
 //        http://wwwdev.ebi.ac.uk/cellbase/webservices/rest/v3/hsapiens/genomic/variant/2:114340663:GCTGGGCATCCT:ACTGGGCATCCT/full_annotation
+        variantAnnotationDBAdaptor.getAllConsequenceTypesByVariant(new GenomicVariant("15", 78224189, "-", "C"), new QueryOptions());  // should
 //        variantAnnotationDBAdaptor.getAllConsequenceTypesByVariant(new GenomicVariant("13", 52718051, "-", "T"), new QueryOptions());  // should
 //        variantAnnotationDBAdaptor.getAllConsequenceTypesByVariant(new GenomicVariant("13", 32893271, "A", "G"), new QueryOptions());  // should set functional description "In BC and ovarian cancer; unknown pathological significance; dbSNP:rs4987046" for ENST00000380152
-        QueryResult queryResult = variantAnnotationDBAdaptor.getAllConsequenceTypesByVariant(new GenomicVariant("19", 45411941, "T", "C"), new QueryOptions());  // should return intergenic_variant
+//        QueryResult queryResult = variantAnnotationDBAdaptor.getAllConsequenceTypesByVariant(new GenomicVariant("19", 45411941, "T", "C"), new QueryOptions());  // should return intergenic_variant
 //        variantAnnotationDBAdaptor.getAllConsequenceTypesByVariant(new GenomicVariant("10", 6638139, "A", "T"), new QueryOptions());  // should return intergenic_variant
 //        variantAnnotationDBAdaptor.getAllConsequenceTypesByVariant(new GenomicVariant("10", 108309064, StringUtils.repeat("N",2252), "-"), new QueryOptions());  // should return ENSG00000215002 ENST00000399415 -       transcript_ablation
 //        variantAnnotationDBAdaptor.getAllConsequenceTypesByVariant(new GenomicVariant("10", 124814698, StringUtils.repeat("N",1048), "-"), new QueryOptions());  // should return ENSG00000215002 ENST00000399415 -       transcript_ablation
