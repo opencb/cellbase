0202e0b5eb012ce96947f1ca9a3d76f53c88649c
Javi Lopez <javild@gmail.com>
2016-02-25 09:54:48 +0000

    feature/annotatevariation: VariationDataReader now skips cases like 1:34354564:4:5

--- cellbase-core/src/main/java/org/opencb/cellbase/core/variant/annotation/VariationDataReader.java
+++ cellbase-core/src/main/java/org/opencb/cellbase/core/variant/annotation/VariationDataReader.java
@@ -1,5 +1,6 @@
 package org.opencb.cellbase.core.variant.annotation;
 
+import org.apache.commons.lang.math.NumberUtils;
 import org.opencb.biodata.models.variant.Variant;
 import org.opencb.cellbase.core.api.VariantDBAdaptor;
 import org.opencb.commons.datastore.core.Query;
@@ -41,10 +42,20 @@ public class VariationDataReader implements DataReader<Variant> {
         return this.iterator != null;
     }
 
+    /**
+     * Performs one read of from a CellBase variation collection.
+     *
+     * @return  List of variants. It can be expected to contain only one variant.
+     */
     public List<Variant> read() {
 
-        if (iterator.hasNext()) {
-            Variant variant = iterator.next();
+        Variant variant = null;
+        boolean valid = false;
+        while (iterator.hasNext() && !valid) {
+            variant = iterator.next();
+            valid = isValid(variant);
+        }
+        if (valid) {
             // New variants in the variation collection created during the update of the frequencies may not have
             // the variant type set and this might cause NPE
             if (variant.getType() == null) {
@@ -55,6 +66,18 @@ public class VariationDataReader implements DataReader<Variant> {
         } else {
             return null;
         }
+    }
+
+    /**
+     * Checks whether a variant is valid
+     *
+     * @param variant Variant object to be checked.
+     * @return   true/false depending on whether 'variant' does contain valid values. Currently just a simple check of
+     * reference/alternate attributes not being numbers is performed to detect cases such as 1:13123123:4:5. Functionality
+     * of the method may be improved in the future.
+     */
+    private boolean isValid(Variant variant) {
+        return !(NumberUtils.isNumber(variant.getReference()) || NumberUtils.isNumber(variant.getAlternate()));
     }
 
     public List<Variant> read(int batchSize) {
