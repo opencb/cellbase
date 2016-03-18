20cf860e1efdddd8901cf68137254ca2c2e720d7
Javi Lopez <javild@gmail.com>
2016-03-18 11:28:28 +0000

    develop: bug solved at variant-annotation CLI when annotating the variation collection and trying to get the list of chromosomes from the database

--- cellbase-app/src/main/java/org/opencb/cellbase/app/cli/VariantAnnotationCommandExecutor.java
+++ cellbase-app/src/main/java/org/opencb/cellbase/app/cli/VariantAnnotationCommandExecutor.java
@@ -75,7 +75,7 @@ public class VariantAnnotationCommandExecutor extends CommandExecutor {
     private String url;
     private boolean local;
     private boolean cellBaseAnnotation;
-    private String[] chromosomeList;
+    private List<String> chromosomeList;
     private int port;
     private String species;
     private String assembly;
@@ -164,7 +164,6 @@ public class VariantAnnotationCommandExecutor extends CommandExecutor {
             } else {
                 // This will annotate the CellBase Variation collection
                 if (cellBaseAnnotation) {
-                    dbAdaptorFactory = new MongoDBAdaptorFactory(configuration);
                     // TODO: enable this query in the parseQuery method within VariantMongoDBAdaptor
 //                    Query query = new Query("$match",
 //                            new Document("annotation.consequenceTypes", new Document("$exists", 0)));
@@ -202,17 +201,18 @@ public class VariantAnnotationCommandExecutor extends CommandExecutor {
 
         if (variantAnnotationCommandOptions.chromosomeList != null
                 && !variantAnnotationCommandOptions.chromosomeList.isEmpty()) {
-            chromosomeList = variantAnnotationCommandOptions.chromosomeList.split(",");
+            chromosomeList = Arrays.asList(variantAnnotationCommandOptions.chromosomeList.split(","));
             logger.info("Setting chromosomes {} for variant annotation", chromosomeList.toString());
         // If the user does not provide any chromosome, fill chromosomeList with all available chromosomes in the
         // database
         } else {
             logger.info("Getting full list of chromosome names in the database");
+            dbAdaptorFactory = new MongoDBAdaptorFactory(configuration);
             GenomeDBAdaptor genomeDBAdaptor = dbAdaptorFactory.getGenomeDBAdaptor(species, assembly);
             QueryResult queryResult = genomeDBAdaptor.getGenomeInfo(new QueryOptions("include", "chromosomes.name"));
 
             List<Document> chromosomeDocumentList = (List<Document>) ((List<Document>) queryResult.getResult()).get(0).get("chromosomes");
-            List<String> chromosomeList = new ArrayList<>(chromosomeDocumentList.size());
+            chromosomeList = new ArrayList<>(chromosomeDocumentList.size());
             for (Document chromosomeDocument : chromosomeDocumentList) {
                 chromosomeList.add((String) chromosomeDocument.get("name"));
             }
@@ -549,15 +549,6 @@ public class VariantAnnotationCommandExecutor extends CommandExecutor {
             }
         }
 
-        // Annotate variation collection in CellBase
-        cellBaseAnnotation = variantAnnotationCommandOptions.cellBaseAnnotation;
-
-        // The list of chromosomes will only be used if annotating the variation collection
-        if (cellBaseAnnotation) {
-            // This will set chromosomeList with the list of chromosomes to annotate
-            setChromosomeList();
-        }
-
         // Species
         if (variantAnnotationCommandOptions.species != null) {
             species = variantAnnotationCommandOptions.species;
@@ -608,5 +599,17 @@ public class VariantAnnotationCommandExecutor extends CommandExecutor {
                 customFileFields.add(Arrays.asList(fieldString.split(",")));
             }
         }
+
+        // Annotate variation collection in CellBase
+        cellBaseAnnotation = variantAnnotationCommandOptions.cellBaseAnnotation;
+
+        // The list of chromosomes will only be used if annotating the variation collection
+        if (cellBaseAnnotation) {
+            // This will set chromosomeList with the list of chromosomes to annotate
+            setChromosomeList();
+        }
+
     }
+
+
 }
