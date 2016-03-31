1c2b60db8a7b3aa170f694d2d6d0bb18913d63d3
Swaathik <swaathik@gmail.com>
2016-03-31 15:34:30 +0100

    Web: Jsorolla dependency added in index.html

--- cellbase-web/Gruntfile.js
+++ cellbase-web/Gruntfile.js
@@ -18,39 +18,6 @@ module.exports = function(grunt) {
                 }
             }
         },
-        watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint']
-        },
-        concat: {
-            options: {
-                sourceMap: true
-            },
-            vendors: {
-                src: [
-                    './bower_components/webcomponentsjs/webcomponents-lite.js',
-                    './bower_components/underscore/underscore.js',
-                    './bower_components/backbone/backbone.js'
-                ],
-                dest: '<%= build.path %>/vendors.js'
-            },
-            jsorolla: {
-                src: [
-                    './bower_components/webcomponentsjs/webcomponents-lite.js',
-                ],
-                dest: '<%= build.path %>/jsorolla.js'
-            }
-        },
-        uglify: {
-            options: {
-                banner: '/*! CellBase <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-            },
-            dist: {
-                files: {
-                    '<%= build.path %>/vendors.min.js': ['<%= build.path %>/vendors.js']
-                }
-            }
-        },
         copy: {
             dist: {
                 files: [
@@ -70,6 +37,38 @@ module.exports = function(grunt) {
                 ]
             }
         },
+        concat: {
+            options: {
+                sourceMap: true
+            },
+            vendors: {
+                src: [
+                    './bower_components/webcomponentsjs/webcomponents-lite.js',
+                    './bower_components/underscore/underscore.js',
+                    './bower_components/backbone/backbone.js'
+                ],
+                dest: '<%= build.path %>/vendors.js'
+            }
+            // jsorolla: {
+            //     src: [
+            //         './lib//jsorolla/src/lib/clients/cellbase-client.js',
+            //         './lib/jsorolla/src/lib/cache/indexeddb-cache.js',
+            //         '/lib/jsorolla/src/lib/clients/cellbase-client-config.js'
+            //     ],
+            //     dest: '<%= build.path %>/jsorolla.js'
+            // }
+        },
+        uglify: {
+            options: {
+                banner: '/*! CellBase <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    '<%= build.path %>/vendors.min.js': ['<%= build.path %>/vendors.js'],
+                    // '<%= build.path %>/jsorolla.min.js': ['<%= build.path %>/jsorolla.js']
+                }
+            }
+        },
         processhtml: {
             options: {
                 strip: true
@@ -79,6 +78,22 @@ module.exports = function(grunt) {
                     '<%= build.path %>/index.html': ['src/index.html']
                 }
             }
+        },
+        vulcanize: {
+            default: {
+                options: {
+                    // Task-specific options go here.
+                    stripComments: true
+                },
+                files: {
+                    // Target-specific file lists and/or options go here.
+                    '<%= build.path %>/cellbase-web.html': 'src/cellbase-web.html'
+                }
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
         },
         replace: {
             dist: {
@@ -95,30 +110,18 @@ module.exports = function(grunt) {
                     // {expand: true, flatten: true, src: ['<%= build.path %>/cellbase-web.html'], dest: '<%= build.path %>'}
                 ]
             }
-        },
-        vulcanize: {
-            default: {
-                options: {
-                    // Task-specific options go here.
-                    stripComments: true
-                },
-                files: {
-                    // Target-specific file lists and/or options go here.
-                    '<%= build.path %>/cellbase-web.html': 'src/cellbase-web.html'
-                }
-            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-processhtml');
-    grunt.loadNpmTasks('grunt-replace');
     grunt.loadNpmTasks('grunt-vulcanize');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
 
     grunt.registerTask('default', ['clean', 'jshint', 'copy', 'concat', 'uglify', 'processhtml', 'vulcanize']);
     grunt.registerTask('cl', ['clean']);
