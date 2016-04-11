d20e7a76d3e32e7795931c80e2c856b9908b1c3b
melsiddieg <melsiddieg@gmail.com>
2016-04-11 15:25:12 +0100

    major upate adding multiple methods and classes

--- clients/R/Client_plan.R
+++ clients/R/Client_plan.R
@@ -1,26 +1,28 @@
-getCellbase <- function(host, version, species, category, subcategory,ids,resource,filter=NULL,...){
-  ids <- readIds(ids = ids)
-  grls <- createURL(host=NULL, version=NULL, species=NULL, categ=NULL,subcateg=NULL,ids,resource,filter=NULL) 
+fetchCellbase <- function(file=NULL,host=host, version=version, species=species, categ, subcateg,ids,resource,filter=NULL,...){
+  #ids <- ids
+  categ <- paste0(categ,"/",sep="")
+  subcateg <- paste0(subcateg,"/",sep="")
+  if(is.null(file)){
+    ids <- paste0(ids,collapse = ",")
+    ids <- paste0(ids,"/",collapse = "")
+  }else{
+    ids <- readIds(file)
+  }
+  
+  grls <- createURL(host=host, version=version, species=species, categ=categ,subcateg=subcateg,ids=ids,resource=resource,filter=NULL) 
   content <- callREST(grls = grls)
-  cell <- parseResponse(content)
+  cell <- parseResponse(content=content)
   return(cell)
 }
-# cellVarAnnotate <- function(ids,...){
-#   
-#   
-# }
-# cellbase_gene <- function(ids){
-#   
-#   
-# }
-readIds <- function(file=NULL,ids=NULL)
+
+readIds <- function(file=file)
   {
   require(Rsamtools)
   require(pbapply)
   ids<- list()
-  num_iter<- ceiling(R.utils::countLines(file)[[1]]/800)
+  num_iter<- ceiling(R.utils::countLines(file)[[1]]/(batch_size*num_threads))
   #batchSize * numThreads
-  demo <- Rsamtools::TabixFile(file,yieldSize = 800)
+  demo <- Rsamtools::TabixFile(file,yieldSize = batch_size*num_threads)
   tbx <- open(demo)
   i <- 1
   while (i <=num_iter)
@@ -30,7 +32,7 @@ readIds <- function(file=NULL,ids=NULL)
     whim <- lapply(inter, function(x){strsplit(x[1],split = "\t")[[1]][c(1,2,4,5)]})
     whish <- sapply(whim, function(x){paste(x,collapse =":")})
     #hope <- paste(whish,collapse = ",")
-    hope <- split(whish, ceiling(seq_along(whish)/200))
+    hope <- split(whish, ceiling(seq_along(whish)/batch_size))
     ids[[i]] <- hope
     i <- i+1
   }
@@ -39,44 +41,62 @@ readIds <- function(file=NULL,ids=NULL)
 }
   
   #create a list of character vectors of urls
-createURL <- function(host=host,version=version,species=species,categ=categ,subcateg=subcate,ids,resource=resource,filter=list)
+createURL <- function(file=NULL,host=host,version=version,species=species,categ=categ,subcateg=subcateg,ids=ids,resource=resource,filter=NULL)
   {
-  host <- "http://bioinfo.hpc.cam.ac.uk/cellbase/webservices/rest"
-  host <- paste0(host,"/",sep="")
-  version <- "v3"
-  version <- paste0(version,"/",sep="")
-  species <- "hsapiens"
-  species <- paste0(species,"/",sep="")
-  categ <- "genomic"
-  categ <- paste0(categ,"/",sep="")
-  subcateg <- "variant"
-  subcateg <- paste0(subcateg,"/",sep="")
-  resource <- "/full_annotation"
-  filter <- list()
-  gcell <- paste0(host,version,species,categ,subcateg,collapse = "")
-  #gbase <- "http://bioinfo.hpc.cam.ac.uk/cellbase/webservices/rest/v3/hsapiens/genomic/variant/"
-  grls <- list()
-  for(i in seq_along(ids)){
-    hop <- paste(ids[[i]],collapse = ",")
-    tmp <- paste0(gcell,hop,resource,collapse = ",")
-    grls[[i]] <- gsub("chr","",tmp)
-  }
-  return(grls)
-}
-callREST <- function(grls,config=NULL,async=TRUE){
-  require(RCurl)
-  if(async==TRUE){
-    prp <- split(grls,ceiling(seq_along(grls)/4))
-    cat("Preparing The Asynchronus call.............")
-    gs <- pblapply(prp, function(x)unlist(x))
-    cat("Getting the Data...............")
-    content <- pblapply(gs,function(x)getURIAsynchronous(x,perform = Inf))
+  if(is.null(file)){
+    grls <- paste0(host,version,species,categ,subcateg,ids,resource,filter,collapse = "")
     
   }else{
-    content <- pbsapply(grls, function(x)getURI(x))
+    grls <- list()
+    gcl <- paste0(host,version,species,categ,subcateg,collapse = "")
     
+    for(i in seq_along(ids)){
+      hop <- paste(ids[[i]],collapse = ",")
+      tmp <- paste0(gcl,hop,resource,collapse = ",")
+      grls[[i]] <- gsub("chr","",tmp)
+    }
   }
-  return(unlist(content))
+  # host <- "http://bioinfodev.hpc.cam.ac.uk/cellbase-dev-v4.0/webservices/rest/"
+  # #host <- paste0(host,"/",sep="")
+  # version <- "v3"
+  # version <- paste0(version,"/",sep="")
+  # species <- "hsapiens"
+  # species <- paste0(species,"/",sep="")
+  # categ <- "genomic"
+  # categ <- paste0(categ,"/",sep="")
+  # # subcateg <- "variant"
+  # subcateg <- paste0(subcateg,"/",sep="")
+  # ids <- paste0(ids,collapse = ",")
+  # ids <- paste0(ids,"/",collapse = "")
+  # resource <- "/full_annotation"
+  # filter <- list()
+  
+  #grls <- paste0(host,version,species,categ,subcateg,ids,resource,filter,collapse = "")
+  #gbase <- "http://bioinfo.hpc.cam.ac.uk/cellbase/webservices/rest/v3/hsapiens/genomic/variant/"
+
+  return(grls)
+}
+callREST <- function(grls,async=FALSE){
+  require(RCurl)
+  if(is.null(file)){
+    content <- getURI(grls)
+  }else{
+    require(pbapply)
+    if(async==TRUE){
+      prp <- split(grls,ceiling(seq_along(grls)/4))
+      cat("Preparing The Asynchronus call.............")
+      gs <- pblapply(prp, function(x)unlist(x))
+      cat("Getting the Data...............")
+      content <- pblapply(gs,function(x)getURIAsynchronous(x,perform = Inf))
+      
+    }else{
+      content <- pbsapply(grls, function(x)getURI(x))
+      
+    }
+  }
+ 
+  
+  return(content)
 }
 parseResponse <- function(content,parallel=TRUE){
   require(jsonlite)
@@ -86,9 +106,9 @@ parseResponse <- function(content,parallel=TRUE){
     numcores <- 4
     registerDoMC(numcores)
     ### Extracting the content in parallel
-    js <- mclapply(content, function(x)fromJSON(x),mc.cores=numcores)
-    res <- mclapply(js, function(x)x$response$result,mc.cores=numcores)
-    ds <- mclapply(res, function(x)rbind.pages(x),mc.cores=numcores)
+    js <- mclapply(content, function(x)fromJSON(x),mc.cores=num_threads)
+    res <- mclapply(js, function(x)x$response$result,mc.cores=num_threads)
+    ds <- mclapply(res, function(x)rbind.pages(x),mc.cores=num_threads)
     ### Important to get correct merging of dataframe
     names(ds) <- NULL
     ds <- rbind.pages(ds)
@@ -106,6 +126,7 @@ parseResponse <- function(content,parallel=TRUE){
   return(ds)
   
 }
+
 
 
 
--- clients/R/cellbase_object.R
+++ clients/R/cellbase_object.R
@@ -1,7 +1,109 @@
 library(methods)
-cellbase <- setClass("cellbase",slots = c(cellbase="data.frame",config="list"),prototype = prototype(
-  cellbase=data.frame(),
-  config=list(host="http://bioinfo.hpc.cam.ac.uk/cellbase/webservices/rest/",version = "v3/")
-))
-list(host="http://bioinfo.hpc.cam.ac.uk/cellbase/webservices/rest/",version = "v3/")
-cellbase()
+source("Client_plan.R")
+
+CellbaseQuery <- setClass("CellbaseQuery",
+            slots = c(config="list",hosts="vector",species="list",categories="vector"),
+            prototype = prototype(
+  config=list(host="http://bioinfodev.hpc.cam.ac.uk/cellbase-dev-v4.0/webservices/rest/",
+              version = "v4/",species="hsapiens/",batch_size=200,num_threads=4),
+  hosts=c("http://bioinfodev.hpc.cam.ac.uk/cellbase-dev-v4.0/webservices/rest/"),
+  species=list(human="hsapiens",mouse="mmusculus",rat="rnorvegicus",chimp="ptroglodytes",
+               gorilla="ggorilla", orangutan="pabelii",macaque="mmulatta",pig="sscrofa",
+               dog="cfamiliaris",horse="ecaballus",rabbit="ocuniculus",chicken="ggallus",
+               cow="btaurus",cat="fcatus",zebrafish="drerio",fruitfly="dmelanogaster",
+               mosquito="agambiae",worm="celegans",yeast="scerevisiae",
+               malaria_parasite="pfalciparum"),
+  categories=c("Genomic","Feature","Regulatory","Network")
+               )
+  )
+
+CellbaseResult <- setClass("CellbaseResult",slots = c(cellbaseData="data.frame"))
+
+setMethod("show",signature = "CellbaseResult",definition = function(object){
+  cat("An object of class ", class(object), "\n", sep = "")
+  cat(" containing ", nrow(object@cellbaseData), " rows and ",
+      ncol(object@cellbaseData), " columns.\n", sep = "")
+})
+#### Methods for cellbaseQuery
+setMethod("show",signature = "CellbaseQuery",definition = function(object){
+  cat("An object of class ", class(object), "\n", sep = "")
+  cat("| slot config containing the following:",paste(names(object@config),collapse = ","),"\n")
+  cat("| slot hosts containing the following:",object@hosts,"\n")
+  cat("| slot species containing the following:",paste(names(object@species),collapse = ","),"\n")
+  cat("| slot categories containing the following:",paste(object@categories,collapse = ","),"\n")
+})
+setGeneric("getCellbase", function(object, ...) standardGeneric("getCellbase"))
+setMethod("getCellbase", "CellbaseQuery",  definition = function(object,file=NULL,host=NULL, version=NULL, species=NULL, categ, subcateg,ids,resource,filter=NULL,...) {
+  stopifnot(class(object)=="CellbaseQuery")
+  if(is.null(host)){
+    host <- object@config$host
+  }else{
+    host=host
+  }
+  if(is.null(species)){
+    species <- object@config$species
+  }else{
+    species=species
+  }
+  version <- object@config$version
+  categ <- categ
+  subcateg<- subcateg
+  ids <- ids
+  resource <- resource
+  result <- fetchCellbase(file=NULL,host=host, version=version, species=species, categ=categ, subcateg=subcateg,ids=ids,resource=resource,filter=NULL,...)
+  data <- CellbaseResult(cellbaseData=result)
+  return(data)
+}
+)
+setGeneric("getSnpsByGene", function(object, ...) standardGeneric("getSnpsByGene"))
+setMethod("getSnpsByGene", "CellbaseQuery",  definition = function(object,file=NULL,host, version, species, categ, subcateg,ids,resource,filter=NULL,...) {
+  
+    host <- object@config$host
+    version <- object@config$version
+    species <- object@config$species
+    categ <- "feature"
+    subcateg<- "gene"
+    ids <- ids
+    resource <- "snp"
+    result <- fetchCellbase(host, file=NULL,version, species, categ, subcateg,ids,resource,filter=NULL,...)
+    data <- CellbaseResult(cellbaseData=result)
+    return(data)
+  
+ 
+}
+)
+# Need more work
+# setGeneric("annotateVcf", function(object, ...) standardGeneric("annotateVcf"))
+# setMethod("annotateVcf", "CellbaseQuery",  definition = function(object,file=file,host, version, species, categ, subcateg,ids=NULL,resource,filter=NULL,...) {
+#   stopifnot(class(object)=="CellbaseQuery")
+#   host <- object@config$host
+#   version <- object@config$version
+#   species <- object@config$species
+#   batch_size <- object@config$batch_size
+#   num_threads <- object@config$num_threads
+#   categ <- "genomic"
+#   subcateg<- "variant"
+#   ids <- ids
+#   resource <- "/annotation"
+#   result <- fetchCellbase(host, file=file,version, species, categ, subcateg,ids=NULL,resource,filter=NULL,...)
+#   data <- CellbaseResult(cellbaseData=result)
+#   return(data)
+# })
+setGeneric("species<-",
+             function(object, value) standardGeneric("species<-"))
+setMethod("species<-","CellbaseQuery",function(object,value){
+  value =paste0(value,"/",sep="")
+  object@config$species <- value
+  return(object)
+})
+
+#### Methods for cellbaseResult
+setGeneric("cellbaseData", function(object, ...) standardGeneric("cellbaseData"))
+setMethod("cellbaseData", "CellbaseResult",  definition = function(object) object@cellbaseData)
+setMethod("[","CellbaseResult",definition = function(x,i,j,drop="missing")
+{
+  .cellbaseData <- x@cellbaseData[i, j]
+  CellbaseResult(cellbaseData = .cellbaseData)
+})
+
+
