cb73c504f7991ec02c39f71dc0691fcf433d6101
Javier Lopez <javild@gmail.com>
2016-06-22 15:44:31 +0100

    feature/annotatecnvs: first approach imprecise position queries implemented

--- cellbase-mongodb/src/main/java/org/opencb/cellbase/mongodb/impl/VariantMongoDBAdaptor.java
+++ cellbase-mongodb/src/main/java/org/opencb/cellbase/mongodb/impl/VariantMongoDBAdaptor.java
@@ -178,7 +178,14 @@ public class VariantMongoDBAdaptor extends MongoDBAdaptor implements VariantDBAd
         createOrQuery(query, VariantMongoDBAdaptor.QueryParams.GENE.key(), "annotation.consequenceTypes.ensemblGeneId",
                 andBsonList);
         createOrQuery(query, QueryParams.CHROMOSOME.key(), "chromosome", andBsonList);
-        createOrQuery(query, QueryParams.START.key(), "start", andBsonList);
+        if ("true".equals(query.get(QueryParams.IMPRECISE.key()))) {
+            createImprecisePositionQuery(query, QueryParams.START.key(), "sv.ciStartLeft", "sv.ciStartRight",
+                    andBsonList);
+            createImprecisePositionQuery(query, QueryParams.END.key(), "sv.ciEndLeft", "sv.ciEndRight",
+                    andBsonList);
+        } else {
+            createOrQuery(query, QueryParams.START.key(), "start", andBsonList);
+        }
         createOrQuery(query, QueryParams.REFERENCE.key(), "reference", andBsonList);
         createOrQuery(query, QueryParams.ALTERNATE.key(), "alternate", andBsonList);
         createOrQuery(query, VariantMongoDBAdaptor.QueryParams.CONSEQUENCE_TYPE.key(),
@@ -190,6 +197,34 @@ public class VariantMongoDBAdaptor extends MongoDBAdaptor implements VariantDBAd
         } else {
             return new Document();
         }
+    }
+
+    private void createImprecisePositionQuery(Query query, String queryParam, String leftLimitMongoField,
+                                              String righLimitMongoField, List<Bson> andBsonList) {
+        if (query != null && query.getString(queryParam) != null && !query.getString(queryParam).isEmpty()) {
+            List<String> queryValues = query.getAsStringList(queryParam);
+            if (queryValues.size() == 1) {
+                andBsonList.addAll(getPositionWithinIntervalQuery(queryValues.get(0), leftLimitMongoField,
+                        righLimitMongoField));
+            } else {
+                List<Bson> orBsonList = new ArrayList<>(queryValues.size());
+                for (String queryItem : queryValues) {
+                    orBsonList.add(Filters.and(getPositionWithinIntervalQuery(queryItem, leftLimitMongoField,
+                            righLimitMongoField)));
+                }
+                andBsonList.add(Filters.or(orBsonList));
+            }
+        }
+    }
+
+    private List<Bson> getPositionWithinIntervalQuery(String valueString, String leftLimitMongoField,
+                                                String righLimitMongoField) {
+        List<Bson> andBsonList = new ArrayList<>(2);
+        int value = Integer.parseInt(valueString);
+        andBsonList.add(Filters.lte(leftLimitMongoField, value));
+        andBsonList.add(Filters.gte(righLimitMongoField, value));
+
+        return andBsonList;
     }
 
     private QueryResult<Long> updatePopulationFrequencies(List<Document> variantDocumentList) {
