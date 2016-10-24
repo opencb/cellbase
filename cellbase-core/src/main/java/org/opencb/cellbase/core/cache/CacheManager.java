81077d481973c095c4321772f218bfb4298a167f
wbari <wasimbari@gmail.com>
2016-10-24 13:58:10 +0100

    core: fixed pom for Kryo

--- cellbase-core/src/main/java/org/opencb/cellbase/core/cache/CacheManager.java
+++ cellbase-core/src/main/java/org/opencb/cellbase/core/cache/CacheManager.java
@@ -1,5 +1,6 @@
 package org.opencb.cellbase.core.cache;
 
+import com.google.common.collect.Sets;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.opencb.cellbase.core.config.CellBaseConfiguration;
 import org.opencb.commons.datastore.core.Query;
@@ -10,7 +11,6 @@ import org.redisson.Redisson;
 import org.redisson.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.codec.KryoCodec;
-import org.redisson.codec.SerializationCodec;
 import org.redisson.core.RMap;
 
 import java.util.*;
@@ -23,6 +23,7 @@ public class CacheManager {
     private CellBaseConfiguration cellBaseConfiguration;
     private Config redissonConfig;
     private RedissonClient redissonClient;
+    private String codec;
 
 
     public CacheManager() {
@@ -30,18 +31,16 @@ public class CacheManager {
 
     public CacheManager(CellBaseConfiguration configuration) {
 
-        if (configuration != null) {
+        if (configuration != null && configuration.getCache() != null) {
             this.cellBaseConfiguration = configuration;
-            String codec = configuration.getCache().getSerialization();
             redissonConfig = new Config();
             redissonConfig.useSingleServer().setAddress(configuration.getCache().getHost());
+            codec = configuration.getCache().getSerialization();
 
             if ("Kryo".equalsIgnoreCase(codec)) {
                 redissonConfig.setCodec(new KryoCodec());
             } else if ("JSON".equalsIgnoreCase(codec)) {
                 redissonConfig.setCodec(new JsonJacksonCodec());
-            } else if ("JDK".equalsIgnoreCase(codec)) {
-                redissonConfig.setCodec(new SerializationCodec());
             }
 
             this.redissonClient = Redisson.create(redissonConfig);
@@ -55,22 +54,21 @@ public class CacheManager {
         long start = System.currentTimeMillis();
         RMap<Integer, Map<String, Object>> map = redissonClient.getMap(key);
         Map<Integer, Map<String, Object>> result = new HashMap<Integer, Map<String, Object>>();
-        Set<Integer> set = new HashSet<Integer>();
-        set.add(0);
+        Set<Integer> set = new HashSet<Integer>(Arrays.asList(0));
+
         result = map.getAll(set);
+
         if (!result.isEmpty()) {
             Object queryResult= result.get(0).get("result");
             QueryResult<T> queryResult1 = (QueryResult<T>) queryResult;
             queryResult1.setDbTime((int) (System.currentTimeMillis() - start));
-            queryResult1.setWarningMsg("Cached!!  :-)");
             return queryResult1;
         }
-        //if empty return null else type cast object
         return null;
     }
 
     public void set(String key, Query query, QueryResult queryResult) {
-        RMap<Integer, Map<String, Object>> map =redissonClient.getMap(key);
+        RMap<Integer, Map<String, Object>> map = redissonClient.getMap(key);
         Map<String, Object> record = new HashMap<String, Object>();
         record.put("query", query);
         record.put("result", queryResult);
@@ -78,31 +76,21 @@ public class CacheManager {
     }
 
     public String createKey(String species, String subcategory, Query query, QueryOptions queryOptions) {
+
+        queryOptions.remove("cache");
         StringBuilder key = new StringBuilder("cb:");
         key.append(cellBaseConfiguration.getVersion()).append(":").append(species).append(":")
                     .append(subcategory);
         SortedMap<String, SortedSet<Object>> map = new TreeMap<String, SortedSet<Object>>();
 
-            // TODO: remove cache from options
+        for (String item: Sets.union(query.keySet(), queryOptions.keySet())) {
+            map.put(item.toLowerCase(), new TreeSet<Object>(query.getAsStringList(item)));
+        }
 
-            for (String item: query.keySet()) {
-                map.put(item.toLowerCase(), new TreeSet<Object>(Arrays.asList(query.get(item))));
-            }
+        String sha1 = DigestUtils.sha1Hex(map.toString());
+        key.append(":").append(sha1);
+        queryOptions.add("cache", "true");
 
-
-            queryOptions.remove("cache");
-            for (String item: queryOptions.keySet()) {
-//                List<Serin> objects = Arrays.asList(queryOptions.getAsStringList(item));
-                map.put(item.toLowerCase(), new TreeSet<Object>(queryOptions.getAsStringList(item)));
-            }
-
-            String sha1 = DigestUtils.sha1Hex(map.toString());
-            key.append(":").append(sha1);
-            // cellBase
-            // version We get from CellBaseConfiguration
-            // CB:version:species:collection
-            // Sort query and queryOptions
-            // get SHA1
         return key.toString();
     }
 }
