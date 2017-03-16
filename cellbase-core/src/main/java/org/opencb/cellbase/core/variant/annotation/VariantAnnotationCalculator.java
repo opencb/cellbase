7395b9d6e3270d666c1606730e9c4baae4051ae2
Javier Lopez <javild@gmail.com>
2017-03-16 18:05:26 +0000

    release-4.5.0-rc.1: some tests added for MNV annotation. CellBase version upgraded at pom.xml files. Biodata version upgraded

--- cellbase-core/src/main/java/org/opencb/cellbase/core/variant/annotation/VariantAnnotationCalculator.java
+++ cellbase-core/src/main/java/org/opencb/cellbase/core/variant/annotation/VariantAnnotationCalculator.java
@@ -963,9 +963,11 @@ public class VariantAnnotationCalculator { //extends MongoDBAdaptor implements V
             if (variant.getReference().isEmpty()) {
 //                variant.setType(VariantType.INSERTION);
                 return VariantType.INSERTION;
-            } else {
+            } else if (variant.getAlternate().isEmpty()) {
 //                variant.setType(VariantType.DELETION);
                 return VariantType.DELETION;
+            } else {
+                return VariantType.MNV;
             }
         }
         return variant.getType();
