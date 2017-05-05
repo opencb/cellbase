da79d70178329697d78de9dff551748dc1cab5b6
dapregi <dperezgil89@gmail.com>
2017-05-05 17:59:42 +0100

    Added repeats parsing

--- cellbase-app/src/main/java/org/opencb/cellbase/app/transform/RepeatsParser.java
+++ cellbase-app/src/main/java/org/opencb/cellbase/app/transform/RepeatsParser.java
@@ -10,12 +10,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 /**
  * Created by fjlopez on 05/05/17.
  */
 public class RepeatsParser extends CellBaseParser {
     private static final String TRF = "trf";
+    private static final String GSD = "genomicSuperDup";
+    private static final String WM = "windowMasker";
     private final Path filesDir;
 
     public RepeatsParser(Path filesDir, CellBaseFileSerializer serializer) {
@@ -38,8 +41,8 @@ public class RepeatsParser extends CellBaseParser {
             parseGsdFile(filesDir.resolve(EtlCommons.GSD_FILE));
         } else {
             logger.warn("No Genomic Super Duplications file found {}", EtlCommons.GSD_FILE);
-            logger.warn("Skipping Genomic Super Duplications file parsing. " +
-                    "Genomic Super Duplicationsata models will not be built.");
+            logger.warn("Skipping Genomic Super Duplications file parsing. "
+                    + "Genomic Super Duplicationsata models will not be built.");
         }
 
         if (Files.exists(filesDir.resolve(EtlCommons.WM_FILE))) {
@@ -49,21 +52,6 @@ public class RepeatsParser extends CellBaseParser {
             logger.warn("Skipping WindowMasker file parsing. WindowMasker data models will not be built.");
         }
 
-    }
-
-    private void parseTrfFile(Path filePath) throws IOException {
-        try (BufferedReader bufferedReader = FileUtils.newBufferedReader(filePath)) {
-            String line = bufferedReader.readLine();
-
-            //            ProgressLogger progressLogger = new ProgressLogger("Parsed TRF lines:");
-            ProgressLogger progressLogger = new ProgressLogger("Parsed TRF lines:", () -> countFileLines(filePath), 200)
-                    .setBatchSize(10000);
-            while (line != null) {
-                serializer.serialize(parseTrfLine(line));
-                line = bufferedReader.readLine();
-                progressLogger.increment(1);
-            }
-        }
     }
 
     private Long countFileLines(Path filePath) throws IOException {
@@ -79,6 +67,20 @@ public class RepeatsParser extends CellBaseParser {
 
     }
 
+    private void parseTrfFile(Path filePath) throws IOException {
+        try (BufferedReader bufferedReader = FileUtils.newBufferedReader(filePath)) {
+            String line = bufferedReader.readLine();
+
+            ProgressLogger progressLogger = new ProgressLogger("Parsed TRF lines:",
+                    () -> countFileLines(filePath), 200).setBatchSize(10000);
+            while (line != null) {
+                serializer.serialize(parseTrfLine(line));
+                line = bufferedReader.readLine();
+                progressLogger.increment(1);
+            }
+        }
+    }
+
     private Repeat parseTrfLine(String line) {
         String[] parts = line.split("\t");
 
@@ -91,21 +93,56 @@ public class RepeatsParser extends CellBaseParser {
         try (BufferedReader bufferedReader = FileUtils.newBufferedReader(filePath)) {
             String line = bufferedReader.readLine();
 
-            Callable<Long> callable = () -> 200L;
-            ProgressLogger progressLogger = new ProgressLogger("Parsed GSD lines:", callable, 300);
+            ProgressLogger progressLogger = new ProgressLogger("Parsed GSD lines:",
+                    () -> countFileLines(filePath), 200).setBatchSize(10000);
+            ArrayList<Repeat> duplications;
             while (line != null) {
-                serializer.serialize(parseGSDLine(line));
+                duplications = parseGSDLine(line);
+                serializer.serialize(duplications.get(0));
+                serializer.serialize(duplications.get(1));
                 line = bufferedReader.readLine();
                 progressLogger.increment(1);
             }
         }
     }
 
-    private Repeat parseGSDLine(String line) {
+    private ArrayList<Repeat> parseGSDLine(String line) {
+        String[] parts = line.split("\t");
+
+        Repeat dupFirst = new Repeat(parts[11], parts[1], Integer.valueOf(parts[2]), Integer.valueOf(parts[3]),
+                null, 2f, Float.valueOf(parts[26]), Float.valueOf(parts[5]),
+                null, GSD);
+
+        Repeat dupSecond = new Repeat(parts[11], parts[6], Integer.valueOf(parts[7]), Integer.valueOf(parts[8]),
+                null, 2f, Float.valueOf(parts[26]), Float.valueOf(parts[5]),
+                null, GSD);
+
+        ArrayList<Repeat> dups = new ArrayList<Repeat>();
+        dups.add(dupFirst);
+        dups.add(dupSecond);
+
+        return dups;
+    }
+
+    private void parseWmFile(Path filePath) throws IOException {
+        try (BufferedReader bufferedReader = FileUtils.newBufferedReader(filePath)) {
+            String line = bufferedReader.readLine();
+
+            ProgressLogger progressLogger = new ProgressLogger("Parsed WM lines:",
+                    () -> countFileLines(filePath), 200).setBatchSize(10000);
+            while (line != null) {
+                serializer.serialize(parseWmLine(line));
+                line = bufferedReader.readLine();
+                progressLogger.increment(1);
+            }
+        }
+    }
+
+    private Repeat parseWmLine(String line) {
         String[] parts = line.split("\t");
 
         return new Repeat(null, parts[1], Integer.valueOf(parts[2]), Integer.valueOf(parts[3]),
-                Integer.valueOf(parts[5]), Float.valueOf(parts[6]), Float.valueOf(parts[8]), Float.valueOf(parts[10]),
-                parts[16], TRF);
+                null, null, null, null,
+                null, WM);
     }
 }
