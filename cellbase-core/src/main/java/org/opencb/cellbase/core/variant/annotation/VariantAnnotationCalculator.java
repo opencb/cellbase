96578447afbbafbb6045245ffe1b11537f520f1e
Javier Lopez <javild@gmail.com>
2017-10-04 15:04:18 +0100

    develop: hgvs bug fixed when run from VariantAnnotationCalculator

--- cellbase-core/src/main/java/org/opencb/cellbase/core/variant/annotation/VariantAnnotationCalculator.java
+++ cellbase-core/src/main/java/org/opencb/cellbase/core/variant/annotation/VariantAnnotationCalculator.java
@@ -961,6 +961,7 @@ public class VariantAnnotationCalculator {
                 + "transcripts.cdnaCodingEnd,transcripts.exons.start,transcripts.exons.cdsStart,transcripts.exons.end,"
                 + "transcripts.exons.cdsEnd,transcripts.exons.sequence,transcripts.exons.phase,"
                 + "transcripts.exons.exonNumber,mirna.matures,mirna.sequence,mirna.matures.cdnaStart,"
+                + "transcripts.exons.genomicCodingStart,transcripts.exons.genomicCodingEnd,"
                 + "mirna.matures.cdnaEnd";
 
         if (annotatorSet.contains("expression")) {
