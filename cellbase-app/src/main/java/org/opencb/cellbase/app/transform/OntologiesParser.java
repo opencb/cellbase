cbbcc3179deaa623fc621e7e3ec5217c358f530f
Javier Lopez <javild@gmail.com>
2017-10-05 17:17:23 +0100

    feature-ontologies: half way implementation of Gene Ontology parser

--- cellbase-app/src/main/java/org/opencb/cellbase/app/transform/OntologiesParser.java
+++ cellbase-app/src/main/java/org/opencb/cellbase/app/transform/OntologiesParser.java
@@ -2,20 +2,28 @@ package org.opencb.cellbase.app.transform;
 
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.*;
+import org.opencb.biodata.models.core.OntologyTerm;
 import org.opencb.cellbase.app.cli.EtlCommons;
 import org.opencb.cellbase.core.serializer.CellBaseFileSerializer;
+import org.opencb.commons.ProgressLogger;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by fjlopez on 20/09/17.
  */
 public class OntologiesParser extends CellBaseParser {
+    private static final char UNDERSCORE = '_';
+    private static final char COLON = ':';
     private final Path filesDir;
+    private final static String SUBCLASSOF = "subClassOf";
+    private final static String IAO_0000115 = "IAO_0000115";
 
     public OntologiesParser(Path filesDir, CellBaseFileSerializer serializer) {
         super(serializer);
@@ -39,26 +47,48 @@ public class OntologiesParser extends CellBaseParser {
         FileReader fr;
         f = new File(filePath.toString());
         fr = new FileReader(f);
+        logger.info("Parsing Gene Ontology...");
         logger.info("Creating ontology model...");
         Model model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
         logger.info("Parsing file into ontology model");
         model.read(fr, null);
 
+        ProgressLogger progressLogger = new ProgressLogger("Parsed terms:", model.size(), 200);
         NodeIterator nodeIterator = model.listObjects();
-        RDFNode rdfNode = nodeIterator.next();
-        logger.info(rdfNode.asResource().getLocalName());
-        StmtIterator stmtIterator = rdfNode.asResource().listProperties();
+        while (nodeIterator.hasNext()) {
+            RDFNode rdfNode = nodeIterator.next();
+            OntologyTerm term = parseTermData(rdfNode.asResource());
+            serializer.serialize(term);
+            progressLogger.increment(1);
+        }
+    }
+
+    private OntologyTerm parseTermData(Resource resource) {
+        OntologyTerm ontologyTerm = new OntologyTerm();
+        StmtIterator stmtIterator = resource.listProperties();
+        List<String> parentList = new ArrayList<>();
         while (stmtIterator.hasNext()) {
             Statement statement = stmtIterator.nextStatement();
+            switch (statement.getPredicate().getLocalName()) {
+                case SUBCLASSOF:
+                    if (statement.getResource() != null && statement.getResource().getLocalName() != null) {
+                        parentList.add(statement.getResource().getLocalName().replace(UNDERSCORE, COLON));
+                    }
+                    break;
+                case IAO_0000115:
+                    ontologyTerm.setDefinition(statement.getString());
+                    break;
+
+            }
+            term.setId();
             if (statement.getPredicate().getLocalName().equals("subClassOf")) {
                 logger.info("{}: {}", statement.getPredicate().getLocalName(), statement.getResource().getLocalName());
             } else {
                 logger.info("{}: {}", statement.getPredicate().getLocalName(), statement.getString());
             }
-//            Literal literal = statement.getLiteral();
-//            literal.
-//            logger.info(statement.getString());
         }
+        ontologyTerm.setSubClassOf(parentList);
+
         //        logger.info(rdfNode.asResource().getId().toString());
 //        logger.info(rdfNode.asResource().getNameSpace());
 //        logger.info(rdfNode.asResource().getProperty(model.createProperty("id")).getString());
@@ -72,4 +102,5 @@ public class OntologiesParser extends CellBaseParser {
 //        }
 
     }
+
 }
