4d5c1aedad0004d52624f02aa23d8439fd776332
imedina <igmecas@gmail.com>
2017-10-09 14:58:44 +0100

    client: add few convenient methods to VariantClient, also deprecate and rename others

--- cellbase-client/src/main/java/org/opencb/cellbase/client/rest/VariantClient.java
+++ cellbase-client/src/main/java/org/opencb/cellbase/client/rest/VariantClient.java
@@ -5,9 +5,12 @@ import org.opencb.biodata.models.variant.avro.VariantAnnotation;
 import org.opencb.cellbase.client.config.ClientConfiguration;
 import org.opencb.commons.datastore.core.QueryOptions;
 import org.opencb.commons.datastore.core.QueryResponse;
+import org.opencb.commons.datastore.core.QueryResult;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -21,19 +24,69 @@ public final class VariantClient extends FeatureClient<Variant> {
         this.subcategory = "variant";
     }
 
-    public QueryResponse<VariantAnnotation> getAnnotations(String id, QueryOptions options) throws IOException {
-        return getAnnotations(Arrays.asList(id.split(",")), options, false);
+    @Deprecated
+    public QueryResponse<VariantAnnotation> getAnnotationByVariantIds(String id, QueryOptions options) throws IOException {
+        return this.getAnnotationByVariantIds(Arrays.asList(id.split(",")), options, false);
     }
 
-    public QueryResponse<VariantAnnotation> getAnnotations(String id, QueryOptions options, boolean post) throws IOException {
-        return getAnnotations(Arrays.asList(id.split(",")), options, post);
+    @Deprecated
+    public QueryResponse<VariantAnnotation> getAnnotationByVariantIds(String id, QueryOptions options, boolean post) throws IOException {
+        return this.getAnnotationByVariantIds(Arrays.asList(id.split(",")), options, post);
     }
 
-    public QueryResponse<VariantAnnotation> getAnnotations(List<String> ids, QueryOptions options) throws IOException {
-        return getAnnotations(ids, options, false);
+    public QueryResponse<VariantAnnotation> getAnnotationByVariantIds(List<String> ids, QueryOptions options) throws IOException {
+        return this.getAnnotationByVariantIds(ids, options, false);
     }
 
-    public QueryResponse<VariantAnnotation> getAnnotations(List<String> ids, QueryOptions options, boolean post) throws IOException {
+    public QueryResponse<VariantAnnotation> getAnnotationByVariantIds(List<String> ids, QueryOptions options, boolean post)
+            throws IOException {
         return execute(ids, "annotation", options, VariantAnnotation.class, post);
     }
+
+
+    public QueryResponse<Variant> annotate(List<Variant> variants, QueryOptions options) throws IOException {
+        return annotate(variants, options, false);
+    }
+
+    public QueryResponse<Variant> annotate(List<Variant> variants, QueryOptions options, boolean post) throws IOException {
+        List<String> variantIds = getVariantAnnotationIds(variants);
+        QueryResponse<VariantAnnotation> annotations = this.getAnnotationByVariantIds(variantIds, options, post);
+
+        int timePerId = annotations.getTime() / variants.size();
+        List<QueryResult<Variant>> annotatedVariants = new ArrayList<>(variants.size());
+        for (int i = 0; i < variants.size(); i++) {
+            variants.get(i).setAnnotation(annotations.getResponse().get(i).first());
+            annotatedVariants.add(new QueryResult<>(variantIds.get(i), timePerId, 1, 1, "", "",
+                    Collections.singletonList(variants.get(i))));
+        }
+
+        return new QueryResponse<>(configuration.getVersion(), annotations.getTime(), options, annotatedVariants);
+    }
+
+    public QueryResponse<VariantAnnotation> getAnnotation(List<Variant> variants, QueryOptions options) throws IOException {
+        return getAnnotation(variants, options, false);
+    }
+
+    public QueryResponse<VariantAnnotation> getAnnotation(List<Variant> variants, QueryOptions options, boolean post) throws IOException {
+        return execute(getVariantAnnotationIds(variants), "annotation", options, VariantAnnotation.class, post);
+    }
+
+
+    // FIXME Next two methods should be moved near the Variant Annotation tool
+    public String getVariantAnnotationId(Variant variant) {
+        return variant.getChromosome() + ":" + variant.getStart() + ":" + variant.getReference() + ":" + variant.getAlternate();
+    }
+
+    public List<String> getVariantAnnotationIds(List<Variant> variants) {
+        if (variants == null) {
+            return null;
+        }
+
+        List<String> variantIds = new ArrayList<>(variants.size());
+        for (Variant variant: variants) {
+            variantIds.add(getVariantAnnotationId(variant));
+        }
+        return variantIds;
+    }
+
 }
