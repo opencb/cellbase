49f5735ce875e07a159abcae552a468d3d628bdf
Javier Lopez <javild@gmail.com>
2017-10-10 11:57:55 +0100

    feature-ontologies: OntologiesParserTests fixed. First run of whole go.owl parsing done. Still contains bugs

--- cellbase-app/src/main/java/org/opencb/cellbase/app/transform/OntologiesParser.java
+++ cellbase-app/src/main/java/org/opencb/cellbase/app/transform/OntologiesParser.java
@@ -1,7 +1,10 @@
 package org.opencb.cellbase.app.transform;
 
+import org.apache.jena.ontology.OntClass;
+import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.*;
+import org.apache.jena.util.iterator.ExtendedIterator;
 import org.opencb.biodata.models.core.OntologyTerm;
 import org.opencb.cellbase.app.cli.EtlCommons;
 import org.opencb.cellbase.core.serializer.CellBaseFileSerializer;
@@ -22,14 +25,14 @@ public class OntologiesParser extends CellBaseParser {
     private static final char UNDERSCORE = '_';
     private static final char COLON = ':';
     private final Path filesDir;
-    private final static String SUBCLASSOF = "subClassOf";
-    private final static String IAO_0000115 = "IAO_0000115";
-    private final static String HASOBONAMESPACE = "hasOBONamespace";
-    private final static String ID = "id";
-    private final static String LABEL = "label";
-    private final static String HASDBXREF = "hasDbXref";
-    private final static String HASEXACTSYNONYM = "hasExactSynonym";
-    private final static String HASALTERNATIVEID = "hasAlternativeId";
+    private static final String SUBCLASSOF = "subClassOf";
+    private static final String IAO_0000115 = "IAO_0000115";
+    private static final String HASOBONAMESPACE = "hasOBONamespace";
+    private static final String ID = "id";
+    private static final String LABEL = "label";
+    private static final String HASDBXREF = "hasDbXref";
+    private static final String HASEXACTSYNONYM = "hasExactSynonym";
+    private static final String HASALTERNATIVEID = "hasAlternativeId";
 
     public OntologiesParser(Path filesDir, CellBaseFileSerializer serializer) {
         super(serializer);
@@ -52,15 +55,15 @@ public class OntologiesParser extends CellBaseParser {
         FileReader fr = new FileReader(new File(filePath.toString()));
         logger.info("Parsing Gene Ontology...");
         logger.info("Creating ontology model...");
-        Model model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
+        OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
         logger.info("Parsing file into ontology model");
         model.read(fr, null);
 
         ProgressLogger progressLogger = new ProgressLogger("Parsed terms:", model.size(), 200);
-        NodeIterator nodeIterator = model.listObjects();
-        while (nodeIterator.hasNext()) {
-            RDFNode rdfNode = nodeIterator.next();
-            OntologyTerm ontologyTerm = parseTermData(rdfNode.asResource());
+        ExtendedIterator<OntClass> iterator = model.listClasses();
+        while (iterator.hasNext()) {
+            OntClass ontClass = iterator.next();
+            OntologyTerm ontologyTerm = parseTermData(ontClass.asResource());
             serializer.serialize(ontologyTerm);
             progressLogger.increment(1);
         }
@@ -101,6 +104,8 @@ public class OntologiesParser extends CellBaseParser {
                     break;
                 case HASALTERNATIVEID:
                     alternativeIdList.add(statement.getString());
+                    break;
+                default:
                     break;
             }
 //            if (statement.getPredicate().getLocalName().equals("subClassOf")) {
