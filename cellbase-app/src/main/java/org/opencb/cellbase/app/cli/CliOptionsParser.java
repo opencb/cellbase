cdc0c3b79fbb62c5ea673daffabb6ac1336a23f3
Nolan Nichols <nolan.nichols@gmail.com>
2017-10-22 08:08:50 -0700

    cli: improve help message for load command

--- cellbase-app/src/main/java/org/opencb/cellbase/app/cli/CliOptionsParser.java
+++ cellbase-app/src/main/java/org/opencb/cellbase/app/cli/CliOptionsParser.java
@@ -187,7 +187,9 @@ public class CliOptionsParser {
         public CommonCommandOptions commonOptions = commonCommandOptions;
 
 
-        @Parameter(names = {"-d", "--data"}, description = "Data model type to be loaded, i.e. genome, gene, ...", required = true, arity = 1)
+        @Parameter(names = {"-d", "--data"}, description = "Comma separated list of data to load: genome, genome_info, "
+                + "gene, variation, variation_functional_score, regulation, protein, ppi, conservation, drug, "
+                + "clinical_variants, repeats, svs. 'all' loads everything.", required = true, arity = 1)
         public String data;
 
         @Parameter(names = {"-i", "--input"}, description = "Input directory with the JSON data models to be loaded. Can also be used to specify a" +
