a6da9e7b535f003cac1a419bfe259d0770b8736f
Javi Lopez <javild@gmail.com>
2018-04-11 15:18:15 +0100

    hotfix-4.5.x: continue fixing ClinVar parser

--- cellbase-app/src/test/java/org/opencb/cellbase/app/transform/clinical/variant/ClinicalVariantParserTest.java
+++ cellbase-app/src/test/java/org/opencb/cellbase/app/transform/clinical/variant/ClinicalVariantParserTest.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.util.JSON;
+import org.apache.commons.lang.StringUtils;
 import org.hamcrest.CoreMatchers;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.opencb.biodata.models.variant.Variant;
 import org.opencb.biodata.models.variant.avro.EvidenceEntry;
+import org.opencb.biodata.models.variant.avro.Property;
 import org.opencb.biodata.models.variant.avro.VariantAvro;
 import org.opencb.cellbase.core.serializer.CellBaseJsonFileSerializer;
 import org.opencb.cellbase.core.serializer.CellBaseSerializer;
@@ -42,64 +44,98 @@ public class ClinicalVariantParserTest {
         CellBaseSerializer serializer = new CellBaseJsonFileSerializer(Paths.get("/tmp/"), "clinical_variant", true);
         (new ClinicalVariantParser(clinicalVariantFolder, genomeSequenceFilePath, "GRCh37",  serializer)).parse();
 
-        List<Variant> variantList = loadSerializedVariants("/tmp/clinical_variant.json.gz");
-        assertEquals(9, variantList.size());
+        List<Variant> parsedVariantList = loadSerializedVariants("/tmp/clinical_variant.json.gz");
+        assertEquals(11, parsedVariantList.size());
 
         // This RCV does not have any genomic feature associated with it (Gene). ClinVar record provides GenotypeSet
         // in this case rather than MeasureSet
-        Variant variant = getVariantByAccession(variantList, "RCV000169692");
-        assertNotNull(variant);
+        List<Variant> variantList = getVariantByAccession(parsedVariantList, "RCV000169692");
+        assertNotNull(variantList);
+        assertEquals(2, variantList.size());
+        // First variant in the genotype set
+        Variant variant = variantList.get(0);
+        assertEquals("18", variant.getChromosome());
+        assertEquals(Integer.valueOf(56390278), variant.getStart());
+        assertEquals("A", variant.getReference());
+        assertEquals("G", variant.getAlternate());
+        // One evidenceEntry for the variation record, another for the RCV
+        assertEquals(2, variant.getAnnotation().getTraitAssociation().size());
+        // Check proper variation record is there
         assertThat(variant.getAnnotation().getTraitAssociation().stream()
-                        .map(evidenceEntry -> evidenceEntry.getId()).collect(Collectors.toList()),
-                CoreMatchers.hasItems("RCV000169692"));
+                        .map(evidenceEntryItem -> evidenceEntryItem.getId()).collect(Collectors.toList()),
+                CoreMatchers.hasItems("187140"));
         EvidenceEntry evidenceEntry = getEvidenceEntryByAccession(variant, "RCV000169692");
+        assertNotNull(evidenceEntry);
         assertTrue(evidenceEntry.getGenomicFeatures().isEmpty());
+        // Check it's properly flagged as part of the genotype set
+        Property property = getProperty(evidenceEntry.getAdditionalProperties(), "GenotypeSet");
+        assertNotNull(property.getValue());
+        assertEquals("18:56390321:C:-", property.getValue());
+        // Second variant in the genotype set
+        variant = variantList.get(0);
+        assertEquals("18", variant.getChromosome());
+        assertEquals(Integer.valueOf(56390321), variant.getStart());
+        assertEquals("C", variant.getReference());
+        assertEquals("", variant.getAlternate());
+        // One evidenceEntry for the variation record, another for the RCV
+        assertEquals(2, variant.getAnnotation().getTraitAssociation().size());
+        // Check proper variation record is there
+        assertThat(variant.getAnnotation().getTraitAssociation().stream()
+                        .map(evidenceEntryItem -> evidenceEntryItem.getId()).collect(Collectors.toList()),
+                CoreMatchers.hasItems("187141"));
+        evidenceEntry = getEvidenceEntryByAccession(variant, "RCV000169692");
+        assertNotNull(evidenceEntry);
+        assertTrue(evidenceEntry.getGenomicFeatures().isEmpty());
+        // Check it's properly flagged as part of the genotype set
+        property = getProperty(evidenceEntry.getAdditionalProperties(), "GenotypeSet");
+        assertNotNull(property.getValue());
+        assertEquals("18:56390278:A:G", property.getValue());
 
 
-        variant = getVariantByAccession(variantList, "COSM1193237");
+        variant = getVariantByAccession(parsedVariantList, "COSM1193237");
         assertNotNull(variant);
         assertThat(variant.getAnnotation().getTraitAssociation().stream()
                 .map(evidenceEntryItem -> evidenceEntryItem.getId()).collect(Collectors.toList()),
                 CoreMatchers.hasItems("RCV000148505"));
 
-        variant = getVariantByAccession(variantList, "RCV000148485");
+        variant = getVariantByAccession(parsedVariantList, "RCV000148485");
         assertNotNull(variant);
         assertThat(variant.getAnnotation().getTraitAssociation().stream()
                         .map(evidenceEntryItem -> evidenceEntryItem.getId()).collect(Collectors.toList()),
                 CoreMatchers.hasItems("COSM5745645"));
 
-        variant = getVariantByAccession(variantList, "COSM4059225");
+        variant = getVariantByAccession(parsedVariantList, "COSM4059225");
         assertNotNull(variant);
 
-        variant = getVariantByAccession(variantList, "3259");
+        variant = getVariantByAccession(parsedVariantList, "3259");
         assertEquals(Integer.valueOf(7577545), variant.getStart());
         assertEquals("T", variant.getReference());
         assertEquals("C", variant.getAlternate());
         assertEquals("PMID:0008075648",
                 variant.getAnnotation().getTraitAssociation().get(0).getBibliography().get(0));
 
-        variant = getVariantByAccession(variantList, "5223");
+        variant = getVariantByAccession(parsedVariantList, "5223");
         assertEquals(Integer.valueOf(4), variant.getStart());
         assertEquals("CTTCTCACCCT", variant.getReference());
         assertEquals("", variant.getAlternate());
         assertEquals("PMID:0008479743",
                 variant.getAnnotation().getTraitAssociation().get(0).getBibliography().get(0));
 
-        variant = getVariantByAccession(variantList, "1590");
+        variant = getVariantByAccession(parsedVariantList, "1590");
         assertEquals(Integer.valueOf(7578502), variant.getStart());
         assertEquals("A", variant.getReference());
         assertEquals("G", variant.getAlternate());
         assertEquals("PMID:0002649981",
                 variant.getAnnotation().getTraitAssociation().get(0).getBibliography().get(0));
 
-        variant = getVariantByAccession(variantList, "2143");
+        variant = getVariantByAccession(parsedVariantList, "2143");
         assertEquals(Integer.valueOf(7578406), variant.getStart());
         assertEquals("C", variant.getReference());
         assertEquals("T", variant.getAlternate());
         assertEquals("PMID:0002649981",
                 variant.getAnnotation().getTraitAssociation().get(0).getBibliography().get(0));
 
-        variant = getVariantByAccession(variantList, "1407");
+        variant = getVariantByAccession(parsedVariantList, "1407");
         assertEquals(Integer.valueOf(7578536), variant.getStart());
         assertEquals("T", variant.getReference());
         assertEquals("G", variant.getAlternate());
