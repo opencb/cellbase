003dd8b3959b68c4d8b0bc7dd76008827e96d405
dapregi <dperezgil89@gmail.com>
2018-04-17 14:20:43 +0100

    improved installation

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -1,20 +1,19 @@
-from setuptools import setup
+from distutils.core import setup
+reqs = [
+    'requests',
+    'pyyaml'
+]
 
-setup_kwargs = {
-    'name': 'pycellbase',
-    'version': '0.4.0',
-    'description': 'Python client for CellBase',
-    'long_description': ('This Python package enables to query and obtain'
-                         ' biological information from the exhaustive'
-                         ' RESTful Web service API that has been implemented'
-                         ' for the CellBase database'),
-    'keywords': 'opencb cellbase client',
-    'author': 'Daniel Perez-Gil',
-    'author_email': 'opencb@googlegroups.com',
-    'url': 'https://github.com/opencb/cellbase',
-    'packages': ['pycellbase'],
-    'package_dir': {'pycellbase': 'pycellbase'},
-    'install_requires': ['pyyaml', 'requests']
-}
-
-setup(**setup_kwargs)
+setup(
+    name='pycellbase',
+    version='0.4.0',
+    packages=['pycellbase'],
+    url='https://github.com/opencb/cellbase',
+    author='Daniel Perez-Gil',
+    author_email='opencb@googlegroups.com',
+    description=('This Python package enables to query and obtain'
+                 ' biological information from the exhaustive'
+                 ' RESTful Web service API that has been implemented'
+                 ' for the CellBase database'),
+    install_requires=reqs
+)
