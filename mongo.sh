e5a1a6cd0a4dca58d8cff8ec552f7dfdb2138df9
Julie Sullivan <julie.sullivan@genomicsengland.co.uk>
2019-10-07 11:29:08 +0100

    script to download and install mongo

---
+++ mongo.sh
@@ -0,0 +1,14 @@
+
+
+# public key
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+
+# add repo
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+
+sudo apt-get update
+
+# install mongo
+sudo apt-get install -y mongodb-org
+
+sudo service mongod start
