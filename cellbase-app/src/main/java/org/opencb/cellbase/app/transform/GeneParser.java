d4236c5b941f7ed80a52206ea3b8a7aa5404491e
Julie Sullivan <julie.sullivan@genomicsengland.co.uk>
2019-10-11 14:40:36 +0100

    don't print to the console

--- cellbase-app/src/main/java/org/opencb/cellbase/app/transform/GeneParser.java
+++ cellbase-app/src/main/java/org/opencb/cellbase/app/transform/GeneParser.java
@@ -340,10 +340,11 @@ public class GeneParser extends CellBaseParser {
                     // no strand dependent
                     transcript.setProteinID(gtf.getAttributes().get("protein_id"));
                 }
-                if (gtf.getFeature().equalsIgnoreCase("start_codon")) {
-                    // nothing to do
-                    System.out.println("Empty block, this should be redesigned");
-                }
+//                if (gtf.getFeature().equalsIgnoreCase("start_codon")) {
+//                    // nothing to do
+//                    // this prints out 123,451 times.
+//                    //System.out.println("Empty block, this should be redesigned");
+//                }
                 if (gtf.getFeature().equalsIgnoreCase("stop_codon")) {
                     //                      setCdnaCodingEnd = false; // stop_codon found, cdnaCodingEnd will be set here,
                     //                      no need to set it at the beginning of next feature
