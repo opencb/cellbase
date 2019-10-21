e4ff72d2c9d00a068587063929f4ed0ff2b2cf03
Julie Sullivan <julie.sullivan@gmail.com>
2019-10-21 13:45:08 +0100

    add unit test for tfbs #423

--- cellbase-app/src/test/java/org/opencb/cellbase/app/transform/GeneParserUtilsTest.java
+++ cellbase-app/src/test/java/org/opencb/cellbase/app/transform/GeneParserUtilsTest.java
@@ -18,19 +18,48 @@ package org.opencb.cellbase.app.transform;
 
 
 import org.junit.Test;
+import org.opencb.biodata.formats.feature.gff.Gff2;
 import org.opencb.biodata.models.core.Constraint;
+import org.opencb.biodata.models.core.MiRNAGene;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class GeneParserUtilsTest {
+
+    @Test
+    public void testGetTfbsMap() throws Exception {
+        final String SEQUENCE_NAME = "chr10";
+        final String SOURCE = "RegulatoryBuild TFBS Motifs v13.0";
+        final String FEATURE = "TF_binding_site";
+
+        Path tfbsFile = Paths.get(getClass().getResource("/MotifFeatures.gff.gz").getFile());
+        Map<String, SortedSet<Gff2>> tfbsMap = GeneParserUtils.getTfbsMap(tfbsFile);
+        assertEquals(1, tfbsMap.size());
+        SortedSet<Gff2> features = tfbsMap.get("10");
+        assertEquals(2, features.size());
+
+        Gff2 expectedFeature1 = new Gff2(SEQUENCE_NAME, SOURCE, FEATURE, 10365, 10384, "5.864", "-", ".", "Name=PPARG::RXRA:MA0065.1");
+        Gff2 expectedFeature2 = new Gff2(SEQUENCE_NAME, SOURCE, FEATURE, 10442, 10456, "10.405", "-", ".", "Name=Tr4:MA0504.1");
+
+        Iterator<Gff2> itr = features.iterator();
+        Gff2 actualFeature1 = itr.next();
+        Gff2 actualFeature2 = itr.next();
+
+        assertEquals(expectedFeature1.toString(), actualFeature1.toString());
+        assertEquals(expectedFeature2.toString(), actualFeature2.toString());
+    }
+
     @Test
     public void testGetConstraints() throws Exception {
         Path gnmoadFile = Paths.get(getClass().getResource("/gnomad.v2.1.1.lof_metrics.by_transcript.txt.gz").getFile());
@@ -47,7 +76,6 @@ public class GeneParserUtilsTest {
         List<Constraint> transcriptConstraints = constraints.get("ENST00000600966");
         assertEquals(3, transcriptConstraints.size());
         assertEquals(expected, transcriptConstraints);
-
 
         constraint1 = new Constraint("gnomAD", "pLoF", "oe_mis", 1.0141);
         constraint2 = new Constraint("gnomAD", "pLoF", "oe_syn", 1.0299);
