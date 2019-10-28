86c4a69cda80b6afcf4cf47085e04488915601ff
Julie Sullivan <julie.sullivan@gmail.com>
2019-10-28 15:50:16 +0000

    update create indexes method #454

--- cellbase-lib/src/main/java/org/opencb/cellbase/lib/impl/MongoDBAdaptor.java
+++ cellbase-lib/src/main/java/org/opencb/cellbase/lib/impl/MongoDBAdaptor.java
@@ -17,10 +17,8 @@
 package org.opencb.cellbase.lib.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.MongoClient;
 import com.mongodb.QueryBuilder;
 import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.*;
 import org.bson.*;
 import org.bson.conversions.Bson;
@@ -30,9 +28,7 @@ import org.opencb.commons.datastore.core.Query;
 import org.opencb.commons.datastore.core.QueryOptions;
 import org.opencb.commons.datastore.core.QueryResult;
 import org.opencb.commons.datastore.mongodb.MongoDBCollection;
-import org.opencb.commons.datastore.mongodb.MongoDBConfiguration;
 import org.opencb.commons.datastore.mongodb.MongoDataStore;
-import org.opencb.commons.datastore.mongodb.MongoDBIndexUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -72,8 +68,7 @@ public class MongoDBAdaptor {
 
     public void createIndexes() {
         InputStream resourceAsStream = getClass().getResourceAsStream("/cellbase-indexes.txt");
-        MongoDBIndexUtils.createIndexes(mongoDataStore, mongoDataStore.getDb(),
-                mongoDataStore.getMongoDBConfiguration(), resourceAsStream);
+        MongoDBIndexUtils.createIndexes(mongoDataStore, resourceAsStream);
     }
 
     private void initSpeciesAssembly(String species, String assembly) {
