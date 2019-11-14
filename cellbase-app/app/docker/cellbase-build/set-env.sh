3f01c5f1b468a624f42733a6de3f07e019f481b0
Julie Sullivan <julie.sullivan@gmail.com>
2019-11-14 11:18:36 +0000

    use docker for ensembl scripts #437

--- cellbase-app/app/docker/cellbase-build/set-env.sh
+++ cellbase-app/app/docker/cellbase-build/set-env.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -e 
-
 PERL5LIB=${PERL5LIB}:/opt/bioperl-live
 PERL5LIB=${PERL5LIB}:/opt/ensembl/modules
 PERL5LIB=${PERL5LIB}:/opt/ensembl-compara/modules
@@ -10,9 +8,3 @@ PERL5LIB=${PERL5LIB}:/opt/ensembl-funcgen/modules
 PERL5LIB=${PERL5LIB}:/opt/lib/perl/5.18.2/
 PERL5LIB=${PERL5LIB}:/opt/
 export PERL5LIB
-
-
-#perl /opt/gene_extra_info.pl
-perl /opt/protein_function_prediction_matrices.pl
-perl /opt/genome_info.pl
-
