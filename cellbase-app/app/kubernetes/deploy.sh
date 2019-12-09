73beb021eaf739e828024bfeff69bfed2cd6771d
Marcus Robinson <marcus.robinson@microsoft.com>
2019-12-09 13:04:05 +0000

    working with replicaset (not sharded)

--- cellbase-app/app/kubernetes/deploy.sh
+++ cellbase-app/app/kubernetes/deploy.sh
@@ -1,10 +1,16 @@
+# Note: this is only good for first time install as password and key change each time!
+
 pass=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
+key=$( openssl rand -base64 741)
+deployment_name="cellbase"
 
 helm dependency update ./cellbase
 
-helm upgrade cellbase  ./cellbase \
+helm upgrade $deployment_name  ./cellbase \
     --set deployMongo=true \
+    --set mongodb.replicaSet.enabled=true \
     --set mongodb.auth.enabled=true \
-    --set mongodb.auth.mongodbRootPassword=$pass \
-    --set mongodb.auth.key="$( openssl rand -base64 741)" \
+    --set mongodb.mongodbRootPassword="$pass" \
+    --set settings.CELLBASE_DATABASES_MONGODB_HOST="$deployment_name-mongodb" \
+    --set mongodb.auth.key="$key" \
     --install
