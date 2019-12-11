c627a5cecd9f97606deb625b44cbb62b869e6f1a
imedina <igmecas@gmail.com>
2019-12-11 23:51:33 +0000

    docker: some work and fixes in docker images and build

--- cellbase-app/app/docker/docker-build.sh
+++ cellbase-app/app/docker/docker-build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-BUILD_FOLDER=$2
+## Get cellbase 'build' folder
+BASEDIR=`dirname $0`
+BUILD_FOLDER=`cd "$BASEDIR/.." >/dev/null; pwd`
 
 build () {
   echo "Building images ..."
-  docker build -t opencb/cellbase-base:next   -f $BUILD_FOLDER/docker/cellbase-base/Dockerfile   $BUILD_FOLDER
-  docker build -t opencb/cellbase-rest:next   -f $BUILD_FOLDER/docker/cellbase-rest/Dockerfile   $BUILD_FOLDER
-  docker build -t opencb/cellbase-build:next  -f $BUILD_FOLDER/docker/cellbase-build/Dockerfile  $BUILD_FOLDER
+  docker build -t cellbase-base:next   -f $BUILD_FOLDER/docker/cellbase-base/Dockerfile   $BUILD_FOLDER
+  docker build -t cellbase-rest:next   -f $BUILD_FOLDER/docker/cellbase-rest/Dockerfile   $BUILD_FOLDER
+  docker build -t cellbase-build:next  -f $BUILD_FOLDER/docker/cellbase-build/Dockerfile  $BUILD_FOLDER
 }
 
 if [ $1 = "build" ]; then
