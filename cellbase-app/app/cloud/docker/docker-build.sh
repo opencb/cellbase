de9ecd81165181006ccf4a3a169017373a274868
Julie Sullivan <julie.sullivan@gmail.com>
2019-12-13 17:13:07 +0000

    add tag to FROM to get the correct image base. add user to each image because they aren't inherited. add help and log statements. make tag required. correct script location based on WORKING_DIR. remove build_dir variable because it complicated the script unnecessarily. (we can restore once we are done). #491

--- cellbase-app/app/cloud/docker/docker-build.sh
+++ cellbase-app/app/cloud/docker/docker-build.sh
@@ -1,21 +1,46 @@
 #!/bin/bash
 
-## GET
-TAG="$2"
+#########################
+# The command line help #
+#########################
+display_help() {
+    echo "Usage: {build|push} tag_name [build_folder]" >&2
+    echo
+    echo "   build           Build the three CellBase docker files"
+    echo "   push            Publish the CellBase docker files on DockerHub"
+    echo "   tag_name        Name of tag on GitHub"
+    echo "   build_folder    (optional) absolute path to location of build directory"
+    echo
+    exit 1
+}
 
-## Get cellbase 'build' folder
-if [ -z "$3" ]; then
-  BASEDIR=`dirname $0`
-  BUILD_FOLDER=`cd "$BASEDIR/../.." >/dev/null; pwd`
-else
-  BUILD_FOLDER=$3
+if [[ -z "$1" ]]; then
+  display_help
 fi
 
+if [ -z "$2" ]; then
+  echo "tag is required"
+  display_help
+fi
+
+## tag
+TAG="$2"
+
 build () {
-  echo "Building images ..."
-  docker build -t cellbase-base:$TAG   -f $BUILD_FOLDER/cloud/docker/cellbase-base/Dockerfile   $BUILD_FOLDER
-  docker build -t cellbase-rest:$TAG   -f $BUILD_FOLDER/cloud/docker/cellbase-rest/Dockerfile   $BUILD_FOLDER
-  docker build -t cellbase-build:$TAG  -f $BUILD_FOLDER/cloud/docker/cellbase-build/Dockerfile  $BUILD_FOLDER
+  echo "****************************"
+  echo "Building cellbase-base ..."
+  echo "***************************"
+  docker build -t cellbase-base:$TAG   -f cellbase-app/app/cloud/docker/cellbase-base/Dockerfile .
+
+  echo "***************************"
+  echo "Building cellbase-rest ..."
+  echo "***************************"
+  docker build -t cellbase-rest:$TAG   -f cellbase-app/app/cloud/docker/cellbase-rest/Dockerfile  . --build-arg TAG=$TAG
+
+  echo "***************************"
+  echo "Building cellbase-build ..."
+  echo "***************************"
+  docker build -t cellbase-build:$TAG  -f cellbase-app/app/cloud/docker/cellbase-build/Dockerfile .
 }
 
 if [ $1 = "build" ]; then
@@ -24,8 +49,6 @@ fi
 
 if [ $1 = "push" ]; then
   build
-
   echo "Pushing images to DockerHub..."
 
 fi
-
