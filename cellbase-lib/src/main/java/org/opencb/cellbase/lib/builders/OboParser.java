e6cd02d1d811263261c3427df8279e50880c4b77
Julie Sullivan <julie.sullivan@gmail.com>
2020-03-17 15:57:32 +0000

    add obo parser

---
+++ cellbase-lib/src/main/java/org/opencb/cellbase/lib/builders/OboParser.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2015-2020 OpenCB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package org.opencb.cellbase.lib.builders;
+
+import org.opencb.biodata.models.core.OboTerm;
+import org.opencb.cellbase.core.serializer.CellBaseSerializer;
+import org.opencb.cellbase.lib.EtlCommons;
+import org.opencb.commons.utils.FileUtils;
+
+import java.io.BufferedReader;
+import java.nio.file.Path;
+import java.util.List;
+
+public class OboParser extends CellBaseParser {
+
+    private Path hpoFile;
+    private Path goFile;
+
+    public OboParser(Path oboDirectoryPath, CellBaseSerializer serializer) {
+        super(serializer);
+        hpoFile = oboDirectoryPath.resolve(EtlCommons.HPO_FILE);
+        goFile = oboDirectoryPath.resolve(EtlCommons.GO_FILE);
+    }
+
+    @Override
+    public void parse() throws Exception {
+        BufferedReader bufferedReader = FileUtils.newBufferedReader(hpoFile);
+        org.opencb.biodata.formats.obo.OboParser parser = new org.opencb.biodata.formats.obo.OboParser();
+        List<OboTerm> terms = parser.parseOBO(bufferedReader);
+        for (OboTerm term : terms) {
+            serializer.serialize(term);
+        }
+
+        bufferedReader = FileUtils.newBufferedReader(goFile);
+        terms = parser.parseOBO(bufferedReader);
+        for (OboTerm term : terms) {
+            serializer.serialize(term);
+        }
+
+        serializer.close();
+    }
+}
