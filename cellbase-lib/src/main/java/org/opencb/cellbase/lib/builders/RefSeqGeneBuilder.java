b3dcc7128d36d7eda44bb27d8d17a7d33172ceec
julie-sullivan <julie.sullivan@gmail.com>
2020-07-20 17:01:24 +0100

    first draft of the refseq parser

---
+++ cellbase-lib/src/main/java/org/opencb/cellbase/lib/builders/RefSeqGeneBuilder.java
@@ -0,0 +1,394 @@
+/*
+ * Copyright 2015-2020 OpenCB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opencb.cellbase.lib.builders;
+
+import org.apache.commons.lang.StringUtils;
+import org.opencb.biodata.formats.feature.gtf.Gtf;
+import org.opencb.biodata.formats.feature.gtf.io.GtfReader;
+import org.opencb.biodata.models.core.Exon;
+import org.opencb.biodata.models.core.Gene;
+import org.opencb.biodata.models.core.Transcript;
+import org.opencb.biodata.models.core.Xref;
+import org.opencb.biodata.tools.sequence.FastaIndex;
+import org.opencb.cellbase.core.config.SpeciesConfiguration;
+import org.opencb.cellbase.core.exception.CellbaseException;
+import org.opencb.cellbase.core.serializer.CellBaseSerializer;
+
+import java.nio.file.Path;
+import java.util.*;
+
+public class RefSeqGeneBuilder extends CellBaseBuilder {
+
+    private Map<String, Integer> transcriptDict;
+    private Map<String, Exon> exonDict;
+    private Path gtfFile;
+    private Path fastaFile;
+    private SpeciesConfiguration speciesConfiguration;
+    private static final Map<String, String> REFSEQ_CHROMOSOMES = new HashMap<>();
+    private final String status = "KNOWN";
+    private final String source = "RefSeq";
+    private Gene gene = null;
+    private Transcript transcript = null;
+    private Exon exon = null;
+    private Integer cdna = 1;
+    private Integer cds = 1;
+    private Set<Xref> dbxrefs = new HashSet<>();;
+
+    public RefSeqGeneBuilder(Path refSeqDirectoryPath, SpeciesConfiguration speciesConfiguration, CellBaseSerializer serializer) {
+        super(serializer);
+        this.speciesConfiguration = speciesConfiguration;
+
+        getGtfFileFromDirectoryPath(refSeqDirectoryPath);
+        getFastaFileFromDirectoryPath(refSeqDirectoryPath);
+
+        transcriptDict = new HashMap<>(250000);
+        exonDict = new HashMap<>(8000000);
+    }
+
+    private void getGtfFileFromDirectoryPath(Path geneDirectoryPath) {
+        for (String fileName : geneDirectoryPath.toFile().list()) {
+            if (fileName.endsWith(".gtf") || fileName.endsWith(".gtf.gz")) {
+                gtfFile = geneDirectoryPath.resolve(fileName);
+                break;
+            }
+        }
+    }
+
+    private void getFastaFileFromDirectoryPath(Path geneDirectoryPath) {
+        for (String fileName : geneDirectoryPath.toFile().list()) {
+            if (fileName.endsWith(".fa") || fileName.endsWith(".fa.gz")) {
+                fastaFile = geneDirectoryPath.resolve(fileName);
+                break;
+            }
+        }
+    }
+
+    public void parse() throws Exception {
+        int geneVersion = 1; // FIXME where is the gene version?
+
+        // Preparing the fasta file for fast accessing
+        //FastaIndex fastaIndex = new FastaIndex(fastaFile);
+        FastaIndex fastaIndex = null; // FIXME which fasta to use?
+
+        logger.info("Parsing RefSeq gtf...");
+        GtfReader gtfReader = new GtfReader(gtfFile);
+
+        Gtf gtf;
+        while ((gtf = gtfReader.read()) != null) {
+
+            String chromosome = getSequenceName(gtf.getSequenceName());
+            switch (gtf.getFeature()) {
+                case "gene":
+                    parseGene(gtf, chromosome, geneVersion);
+                    break;
+                case "exon":
+                    parseExon(gtf, chromosome, fastaIndex);
+                    break;
+                case "CDS":
+                    parseCDS(gtf);
+                    break;
+                case "start_codon":
+                    // should I be doing something here?
+                    break;
+                case "stop_codon":
+                    parseStopCodon(gtf, transcript, exon, cdna, cds);
+                    break;
+                default:
+                    throw new RuntimeException("Unexpected feature type: " + gtf.getFeature());
+            }
+        }
+
+        // last gene must be serialized
+        store();
+
+        // cleaning
+        gtfReader.close();
+        serializer.close();
+    }
+
+    // store right before parsing the previous gene, or the very last gene.
+    private void store() {
+        // all genes, transcripts, exons and CDSs have been parsed, so set xrefs here
+        transcript.setXrefs(new ArrayList<>(dbxrefs));
+        serializer.serialize(gene);
+        reset();
+    }
+
+    // Empty transcript and exon dictionaries
+    private void reset() {
+        transcriptDict.clear();
+        exonDict.clear();
+        dbxrefs = new HashSet<>();
+    }
+
+    private void parseStopCodon(Gtf gtf, Transcript transcript, Exon exon, Integer cdna, Integer cds) {
+        if (exon.getStrand().equals("+")) {
+            updateStopCodingDataPositiveExon(exon, cdna, cds, gtf);
+
+            cds += gtf.getEnd() - gtf.getStart();
+            // If stop_codon appears, overwrite values
+            transcript.setGenomicCodingEnd(gtf.getEnd());
+            transcript.setCdnaCodingEnd(gtf.getEnd() - exon.getStart() + cdna);
+            transcript.setCdsLength(cds - 1);
+
+        } else {
+            updateNegativeExonCodingData(exon, cdna, cds, gtf);
+
+            cds += gtf.getEnd() - gtf.getStart();
+            // If stop_codon appears, overwrite values
+            transcript.setGenomicCodingStart(gtf.getStart());
+            // cdnaCodingEnd points to the same base position than genomicCodingStart
+            transcript.setCdnaCodingEnd(exon.getEnd() - gtf.getStart() + cdna);
+            transcript.setCdsLength(cds - 1);
+        }
+    }
+
+    private void parseCDS(Gtf gtf) {
+        exon.setGenomicCodingStart(gtf.getStart());
+        exon.setGenomicCodingEnd(gtf.getEnd());
+
+        exon.setCdsStart(cds);
+        exon.setCdsEnd(gtf.getEnd() - gtf.getStart() + cds);
+
+        transcript.setProteinId(gtf.getAttributes().get("protein_id"));
+
+        exon.setPhase(Integer.valueOf(gtf.getFrame()));
+
+        if (gtf.getStrand().equals("+")) {
+
+            // cDNA coordinates
+            exon.setCdnaCodingStart(gtf.getStart() - exon.getStart() + cdna);
+            exon.setCdnaCodingEnd(gtf.getEnd() - exon.getStart() + cdna);
+
+            // Set cdnaCodingEnd to prevent those cases without stop_codon
+            transcript.setCdnaCodingEnd(gtf.getEnd() - exon.getStart() + cdna);
+
+            if (transcript.getGenomicCodingStart() == 0 || transcript.getGenomicCodingStart() > gtf.getStart()) {
+                transcript.setGenomicCodingStart(gtf.getStart());
+            }
+            if (transcript.getGenomicCodingEnd() == 0 || transcript.getGenomicCodingEnd() < gtf.getEnd()) {
+                transcript.setGenomicCodingEnd(gtf.getEnd());
+            }
+            // only first time
+            if (transcript.getCdnaCodingStart() == 0) {
+                transcript.setCdnaCodingStart(gtf.getStart() - exon.getStart() + cdna);
+            }
+        } else {
+
+            // cDNA coordinates
+            // cdnaCodingStart points to the same base position than genomicCodingEnd
+            exon.setCdnaCodingStart(exon.getEnd() - gtf.getEnd() + cdna);
+            // cdnaCodingEnd points to the same base position than genomicCodingStart
+            exon.setCdnaCodingEnd(exon.getEnd() - gtf.getStart() + cdna);
+
+            // Set cdnaCodingEnd to prevent those cases without stop_codon
+            transcript.setCdnaCodingEnd(exon.getEnd() - gtf.getStart() + cdna);
+
+            if (transcript.getGenomicCodingStart() == 0 || transcript.getGenomicCodingStart() > gtf.getStart()) {
+                transcript.setGenomicCodingStart(gtf.getStart());
+            }
+            if (transcript.getGenomicCodingEnd() == 0 || transcript.getGenomicCodingEnd() < gtf.getEnd()) {
+                transcript.setGenomicCodingEnd(gtf.getEnd());
+            }
+            // only first time
+            if (transcript.getCdnaCodingStart() == 0) {
+                // cdnaCodingStart points to the same base position than genomicCodingEnd
+                transcript.setCdnaCodingStart(exon.getEnd() - gtf.getEnd() + cdna);
+            }
+        }
+
+        // increment in the coding length
+        cds += gtf.getEnd() - gtf.getStart() + 1;
+        transcript.setCdsLength(cds - 1);  // Set cdnaCodingEnd to prevent those cases without stop_codon
+
+    }
+
+    private void updateStopCodingDataPositiveExon(Exon exon, int cdna, int cds, Gtf gtf) {
+        // we need to increment 3 nts, the stop_codon length.
+        exon.setGenomicCodingEnd(gtf.getEnd());
+        exon.setCdnaCodingEnd(gtf.getEnd() - exon.getStart() + cdna);
+        exon.setCdsEnd(gtf.getEnd() - gtf.getStart() + cds);
+
+        // If the STOP codon corresponds to the first three nts of the exon then no CDS will be defined
+        // in the gtf -as technically the STOP codon is non-coding- and we must manually set coding
+        // starts
+        if (exon.getGenomicCodingStart() == 0) {
+            exon.setGenomicCodingStart(exon.getGenomicCodingEnd() - 2);
+        }
+        if (exon.getCdnaCodingStart() == 0) {
+            exon.setCdnaCodingStart(exon.getCdnaCodingEnd() - 2);
+        }
+        if (exon.getCdsStart() == 0) {
+            exon.setCdsStart(exon.getCdsEnd() - 2);
+        }
+    }
+
+    private void updateNegativeExonCodingData(Exon exon, int cdna, int cds, Gtf gtf) {
+        // we need to increment 3 nts, the stop_codon length.
+        exon.setGenomicCodingStart(gtf.getStart());
+        // cdnaCodingEnd points to the same base position than genomicCodingStart
+        exon.setCdnaCodingEnd(exon.getEnd() - gtf.getStart() + cdna);
+        exon.setCdsEnd(gtf.getEnd() - gtf.getStart() + cds);
+
+        // If the STOP codon corresponds to the first three nts of the exon then no CDS will be defined
+        // in the gtf -as technically the STOP codon is non-coding- and we must manually set coding
+        // starts
+        if (exon.getGenomicCodingEnd() == 0) {
+            exon.setGenomicCodingEnd(exon.getGenomicCodingStart() + 2);
+        }
+        if (exon.getCdnaCodingStart() == 0) {
+            exon.setCdnaCodingStart(exon.getCdnaCodingEnd() - 2);
+        }
+        if (exon.getCdsStart() == 0) {
+            exon.setCdsStart(exon.getCdsEnd() - 2);
+        }
+    }
+
+    private void parseGene(Gtf gtf, String chromosome, int geneVersion) throws CellbaseException {
+        // If new geneId is different from the current then we must serialize before data new gene
+        if (gene != null) {
+            store();
+        }
+
+        String geneId = getGeneId(gtf);
+        String geneName = gtf.getAttributes().get("gene_id");
+        String geneDescription = gtf.getAttributes().get("description");
+        String geneBiotype = gtf.getAttributes().get("gene_biotype");
+        gene = new Gene(geneId, geneName, chromosome, gtf.getStart(), gtf.getEnd(), gtf.getStrand(), geneVersion, geneBiotype,
+                status, source, geneDescription, new ArrayList<>(), null, null);
+        parseXrefs(gtf);
+    }
+
+    private void parseXrefs(Gtf gtf) {
+        String xrefs = gtf.getAttributes().get("db_xref");
+        if (StringUtils.isNotEmpty(xrefs)) {
+            for (String xrefString : xrefs.split(",")) {
+                String[] dbxrefParts = xrefString.split(":");
+                String dbName = dbxrefParts[0];
+                String id = dbxrefParts[1];
+                Xref xref = new Xref();
+                xref.setId(id);
+                xref.setDbDisplayName(dbName);
+                xref.setDbName(dbName);
+                dbxrefs.add(xref);
+            }
+        }
+    }
+
+    private void parseExon(Gtf gtf, String chromosome, FastaIndex fastaIndex) {
+        String transcriptId = gtf.getAttributes().get("transcript_id");
+        String transcriptVersion = transcriptId.split("\\.")[1];
+
+        if (!transcriptDict.containsKey(transcriptId)) {
+            transcript = getTranscript(gtf, chromosome, transcriptId, Integer.valueOf(transcriptVersion));
+        } else {
+            transcript = gene.getTranscripts().get(transcriptDict.get(transcriptId));
+        }
+
+//        String exonSequence = fastaIndex.query(gtf.getSequenceName(), gtf.getStart(), gtf.getEnd());
+        String exonSequence = "FIXME-NEED-FASTA"; // FIXME need fasta
+        String exonNumber = gtf.getAttributes().get("exon_number");
+        String exonId = transcriptId + "_" + exonNumber;
+
+        // default value. can be overwritten in the CDS entry
+        int phase = -1;
+
+        exon = new Exon(exonId, chromosome, gtf.getStart(), gtf.getEnd(), gtf.getStrand(), 0, 0, 0, 0, 0, 0, phase,
+                Integer.parseInt(exonNumber), exonSequence);
+        transcript.getExons().add(exon);
+        exonDict.put(transcript.getId() + "_" + exon.getExonNumber(), exon);
+
+        if (gtf.getAttributes().get("exon_number").equals("1")) {
+            cdna = 1;
+            cds = 1;
+        } else {
+            // with every exon we update cDNA length with the previous exon length
+            cdna += exonDict.get(transcript.getId() + "_" + (exon.getExonNumber() - 1)).getEnd()
+                    - exonDict.get(transcript.getId() + "_" + (exon.getExonNumber() - 1)).getStart() + 1;
+        }
+    }
+
+    private Transcript getTranscript(Gtf gtf, String chromosome, String transcriptId, int version) {
+        Map<String, String> gtfAttributes = gtf.getAttributes();
+        String biotype = gtfAttributes.get("gbkey");
+        String transcriptName = gene.getName();
+
+        transcript = new Transcript(transcriptId, transcriptName, biotype, status, source, chromosome, gtf.getStart(), gtf.getEnd(),
+                gtf.getStrand(), version, null, 0, 0, 0, 0,
+                0, "", "", null, new ArrayList<Exon>(),
+                null, null);
+
+        gene.getTranscripts().add(transcript);
+
+        // Do not change order!! size()-1 is the index of the transcript ID
+        transcriptDict.put(transcriptId, gene.getTranscripts().size() - 1);
+        return transcript;
+    }
+
+    private String getGeneId(Gtf gtf) throws CellbaseException {
+        // db_xref "GeneID:100287102";
+        String xrefString = gtf.getAttributes().get("db_xref");
+        String[] xrefs = xrefString.split(",");
+        for (String xref : xrefs) {
+            String[] xrefParts = xref.split(":");
+            if ("GeneID".equals(xrefParts[0])) {
+                return xrefParts[1];
+            }
+        }
+        // didn't find geneId!
+        throw new CellbaseException("Didn't find geneId for db_xref:" + xrefString);
+    }
+
+    private String getSequenceName(String fullSequenceName) {
+        String[] sequenceNameParts = fullSequenceName.split("\\.");
+
+        if (sequenceNameParts.length != 2) {
+            throw new RuntimeException("bad chromosome: " + fullSequenceName);
+        }
+
+        // just get the first part, e.g. NC_000024.11
+        String sequenceName = sequenceNameParts[0];
+        return REFSEQ_CHROMOSOMES.get(sequenceName);
+    }
+
+    static {
+        REFSEQ_CHROMOSOMES.put("NC_000001", "1");
+        REFSEQ_CHROMOSOMES.put("NC_000002", "2");
+        REFSEQ_CHROMOSOMES.put("NC_000003", "3");
+        REFSEQ_CHROMOSOMES.put("NC_000004", "4");
+        REFSEQ_CHROMOSOMES.put("NC_000005", "5");
+        REFSEQ_CHROMOSOMES.put("NC_000006", "6");
+        REFSEQ_CHROMOSOMES.put("NC_000007", "7");
+        REFSEQ_CHROMOSOMES.put("NC_000008", "8");
+        REFSEQ_CHROMOSOMES.put("NC_000009", "9");
+        REFSEQ_CHROMOSOMES.put("NC_000010", "10");
+        REFSEQ_CHROMOSOMES.put("NC_000011", "11");
+        REFSEQ_CHROMOSOMES.put("NC_000012", "12");
+        REFSEQ_CHROMOSOMES.put("NC_000013", "13");
+        REFSEQ_CHROMOSOMES.put("NC_000014", "14");
+        REFSEQ_CHROMOSOMES.put("NC_000015", "15");
+        REFSEQ_CHROMOSOMES.put("NC_000016", "16");
+        REFSEQ_CHROMOSOMES.put("NC_000017", "17");
+        REFSEQ_CHROMOSOMES.put("NC_000018", "18");
+        REFSEQ_CHROMOSOMES.put("NC_000019", "19");
+        REFSEQ_CHROMOSOMES.put("NC_000020", "20");
+        REFSEQ_CHROMOSOMES.put("NC_000021", "21");
+        REFSEQ_CHROMOSOMES.put("NC_000022", "22");
+        REFSEQ_CHROMOSOMES.put("NC_000023", "X");
+        REFSEQ_CHROMOSOMES.put("NC_000024", "Y");
+    }
+}
---
+++ cellbase-lib/src/test/java/org/opencb/cellbase/lib/builders/RefSeqBuilderTest.java
@@ -0,0 +1,217 @@
+/*
+ * Copyright 2015-2020 OpenCB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opencb.cellbase.lib.builders;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mortbay.util.ajax.JSON;
+import org.opencb.biodata.models.core.*;
+import org.opencb.cellbase.core.config.SpeciesConfiguration;
+import org.opencb.cellbase.core.exception.CellbaseException;
+import org.opencb.cellbase.core.serializer.CellBaseFileSerializer;
+import org.opencb.cellbase.core.serializer.CellBaseJsonFileSerializer;
+import org.opencb.cellbase.core.serializer.CellBaseSerializer;
+import org.opencb.commons.utils.FileUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class RefSeqBuilderTest {
+    private RefSeqGeneBuilder geneParser;
+    private ObjectMapper jsonObjectMapper;
+    private static final SpeciesConfiguration SPECIES = new SpeciesConfiguration("hsapiens", "Homo sapiens", "human", null, null, null);
+    public RefSeqBuilderTest() throws Exception {
+        init();
+    }
+
+    @BeforeAll
+    public void init() throws Exception {
+        Path genomeSequenceFastaFile
+                = Paths.get(RefSeqBuilderTest.class.getResource("/gene/Homo_sapiens.GRCh38.fa").toURI());
+        Path geneDirectoryPath = Paths.get(RefSeqBuilderTest.class.getResource("/gene_refseq").toURI());
+        // put the results in /tmp
+        CellBaseSerializer serializer = new CellBaseJsonFileSerializer(Paths.get("/tmp/"), "refseq",
+                true);
+        SpeciesConfiguration species = new SpeciesConfiguration("hsapiens", "Homo sapiens",
+                "human", null, null, null);
+        geneParser = new RefSeqGeneBuilder(geneDirectoryPath, species, serializer);
+        geneParser.parse();
+        jsonObjectMapper = new ObjectMapper();
+        jsonObjectMapper.configure(MapperFeature.REQUIRE_SETTERS_FOR_GETTERS, true);
+        jsonObjectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        serializer.close();
+    }
+
+    @Test
+    public void testParse() throws Exception {
+
+        List<Gene> genes = loadGenes(Paths.get("/tmp/refseq.json.gz"));
+        assertEquals(1, genes.size());
+
+
+        assertEquals(loadGenes(Paths.get(getClass().getResource("/gene_refseq/refseq.json.gz").getFile())),
+                loadGenes(Paths.get("/tmp/refseq.json.gz")));
+    }
+
+    private List<Gene> loadGenes(Path path) throws IOException {
+        List<Gene> genes = new ArrayList<>();
+        ObjectMapper jsonObjectMapper = new ObjectMapper();
+        jsonObjectMapper.configure(MapperFeature.REQUIRE_SETTERS_FOR_GETTERS, true);
+        jsonObjectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        try (BufferedReader bufferedReader = FileUtils.newBufferedReader(path)) {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                genes.add(jsonObjectMapper.convertValue(JSON.parse(line), Gene.class));
+                line = bufferedReader.readLine();
+            }
+        }
+
+        return genes;
+    }
+
+
+    private Xref getXref(Transcript transcript, String xrefId) {
+        for (Xref xref : transcript.getXrefs()) {
+            if (xref.getId().equals(xrefId)) {
+                return xref;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Checks a case in which the stop codon is the first 3 nts of an exon. ENSE00003800362 is in the negative strand.
+     * genomicCodingEnd, cdnaCodingStart and cdsStart must be set "manually" within the parser as there's no CDS line
+     * in the GTF since the stop codon itself is not part of the coding sequence (but historically considered part of
+     * the coding region in CellBase)
+     */
+//    @Test
+//    public void testEdgeExonCodingStart() throws Exception {
+//        geneParser.parse();
+//        List<Gene> genes = loadSerializedGenes("/tmp/gene.json.gz");
+//        Exon exon = getExon("ENSE00003800362", genes);
+//        assertNotNull(exon);
+//        assertEquals(28477630, exon.getGenomicCodingEnd());
+//        assertEquals(1302, exon.getCdnaCodingStart());
+//        assertEquals(1198, exon.getCdsStart());
+//    }
+
+    private Exon getExon(String exonId, List<Gene> genes) {
+        for (Gene gene : genes) {
+            for (Transcript transcript : gene.getTranscripts()) {
+                for (Exon exon : transcript.getExons()) {
+                    if (exonId.equals(exon.getId())) {
+                        return exon;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private Gene getGene(String id, List<Gene> genes) {
+        for (Gene gene : genes) {
+            if (id.equals(gene.getId())) {
+                return gene;
+            }
+        }
+        return null;
+    }
+
+    @Test
+    public void testTranscriptSequenceAndVersion() throws Exception {
+        geneParser.parse();
+        final String TRANSCRIPT_SEQUENCE = "GTTAACTTGCCGTCAGCCTTTTCTTTGACCTCTTCTTTCTGTTCATGTGTATTTGCTGTCTCTTAGCCCAGACTTCCCGTGTCCTTTCCACCGGGCCTTTGAGAGGTCACAGGGTCTTGATGCTGTGGTCTTCATCTGCAGGTGTCTGACTTCCAGCAACTGCTGGCCTGTGCCAGGGTGCAAGCTGAGCACTGGAGTGGAGTTTTCCTGTGGAGAGGAGCCATGCCTAGAGTGGGATGGGCCATTGTTCATCTTCTGGCCCCTGTTGTCTGCATGTAACTTAATACCACAACCAGGCATAGGGGAAAGATTGGAGGAAAGATGAGTGAGAGCATCAACTTCTCTCACAACCTAGGCCAGTGTGTGGTGATGCCAGGCATGCCCTTCCCCAGCATCAGGTCTCCAGAGCTGCAGAAGACGACGGCCGACTTGGATCACACTCTTGTGAGTGTCCCCAGTGTTGCAGAGGCAGGGCCATCAGGCACCAAAGGGATTCTGCCAGCATAGTGCTCCTGGACCAGTGATACACCCGGCACCCTGTCCTGGACACGCTGTTGGCCTGGATCTGAGCCCTGGTGGAGGTCAAAGCCACCTTTGGTTCTGCCATTGCTGCTGTGTGGAAGTTCACTCCTGCCTTTTCCTTTCCCTAGAGCCTCCACCACCCCGAGATCACATTTCTCACTGCCTTTTGTCTGCCCAGTTTCACCAGAAGTAGGCCTCTTCCTGACAGGCAGCTGCACCACTGCCTGGCGCTGTGCCCTTCCTTTGCTCTGCCCGCTGGAGACGGTGTTTGTCATGGGCCTGGTCTGCAGGGATCCTGCTACAAAGGTGAAACCCAGGAGAGTGTGGAGTCCAGAGTGTTGCCAGGACCCAGGCACAGGCATTAGTGCCCGTTGGAGAAAACAGGGGAATCCCGAAGAAATGGTGGGTCCTGGCCATCCGTGAGATCTTCCCAGGGCAGCTCCCCTCTGTGGAATCCAATCTGTCTTCCATCCTGCGTGGCCGAGGGCCAGGCTTCTCACTGGGCCTCTGCAGGAGGCTGCCATTTGTCCTGCCCACCTTCTTAGAAGCGAGACGGAGCAGACCCATCTGCTACTGCCCTTTCTATAATAACTAAAGTTAGCTGCCCTGGACTATTCACCCCCTAGTCTCAATTTAAGAAGATCCCCATGGCCACAGGGCCCCTGCCTGGGGGCTTGTCACCTCCCCCACCTTCTTCCTGAGTCATTCCTGCAGCCTTGCTCCCTAACCTGCCCCACAGCCTTGCCTGGATTTCTATCTCCCTGGCTTGGTGCCAGTTCCTCCAAGTCGATGGCACCTCCCTCCCTCTCAACCACTTGAGCAAACTCCAAGACATCTTCTACCCCAACACCAGCAATTGTGCCAAGGGCCATTAGGCTCTCAGCATGACTATTTTTAGAGACCCCGTGTCTGTCACTGAAACCTTTTTTGTGGGAGACTATTCCTCCCATCTGCAACAGCTGCCCCTGCTGACTGCCCTTCTCTCCTCCCTCTCATCCCAGAGAAACAGGTCAGCTGGGAGCTTCTGCCCCCACTGCCTAGGGACCAACAGGGGCAGGAGGCAGTCACTGACCCCGAGACGTTTGCATCCTGCACAGCTAGAGATCCTTTATTAAAAGCACACTGTTGGTTTCTG";
+        List<Gene> genes = loadSerializedGenes("/tmp/gene.json.gz");
+        Transcript transcript = getTranscript("ENST00000456328", genes);
+        assertNotNull(transcript);
+        assertEquals(TRANSCRIPT_SEQUENCE, transcript.getcDnaSequence());
+        assertEquals(2, transcript.getVersion());
+        assertEquals("havana", transcript.getSource());
+        assertEquals("1", transcript.getSupportLevel());
+    }
+
+    private Transcript getTranscript(Gene gene, String transcriptId) {
+        for (Transcript transcript : gene.getTranscripts()) {
+            if (transcript.getId().equals(transcriptId)) {
+                return transcript;
+            }
+        }
+        return null;
+    }
+
+    private Transcript getTranscript(String transcriptId, List<Gene> geneList) {
+        for (Gene gene : geneList) {
+            return getTranscript(gene, transcriptId);
+        }
+        return null;
+    }
+
+    @Test
+    public void testProteinSequence() throws Exception {
+        geneParser.parse();
+        final String PROTEIN_SEQUENCE = "MVTEFIFLGLSDSQELQTFLFMLFFVFYGGIVFGNLLIVITVVSDSHLHSPMYFLLANLSLIDLSLSSVTAPKMITDFFSQRKVISFKGCLVQIFLLHFFGGSEMVILIAMGFDRYIAICKPLHYTTIMCGNACVGIMAVTWGIGFLHSVSQLAFAVHLLFCGPNEVDSFYCDLPRVIKLACTDTYRLDIMVIANSGVLTVCSFVLLIISYTIILMTIQHRPLDKSSKALSTLTAHITVVLLFFGPCVFIYAWPFPIKSLDKFLAVFYSVITPLLNPIIYTLRNKDMKTAIRQLRKWDAHSSVKF";
+        List<Gene> genes = loadSerializedGenes("/tmp/gene.json.gz");
+        assertEquals(15, genes.size());
+        for (Gene gene : genes) {
+            if (gene.getId().equals("ENSG00000223972")) {
+                for (Transcript transcript : gene.getTranscripts()) {
+                    if (transcript.getId().equals("ENST00000456328")) {
+                        assertEquals(PROTEIN_SEQUENCE, transcript.getProteinSequence());
+                    }
+                }
+                assertEquals("havana", gene.getSource());
+                assertEquals(5, gene.getVersion());
+            }
+        }
+    }
+
+    private List<Gene> loadSerializedGenes(String fileName) {
+        List<Gene> geneList = new ArrayList();
+
+        try {
+            BufferedReader bufferedReader = FileUtils.newBufferedReader(Paths.get(fileName));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.startsWith("#") || line.trim().isEmpty()) {
+                    continue;
+                }
+                geneList.add(jsonObjectMapper.readValue(line, Gene.class));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            assertFalse(false);
+        }
+
+        return geneList;
+    }
+}
