02fd4b246147ffd86e84bf0e8a553de4017dae5f
julie-sullivan <julie.sullivan@gmail.com>
2020-09-23 15:38:51 +0100

    add two config files

--- cellbase-app/app/conf/cellbase-env.sh
+++ cellbase-app/app/conf/cellbase-env.sh
@@ -12,20 +12,19 @@ if [ -e "monitor/dd-java-agent.jar" ]; then
 fi
 
 JAVA_HEAP="2048m"
-LOG_CONFIG="log4j2.xml"
-#CELLBASE_LOG_FILE=${CELLBASE_LOG_FILE:-`grep logFile ` +  ${BASEDIR} + `/conf/configuration.yml | cut -d ':' -f 2`}
 #CELLBASE_LOG_LEVEL=${CELLBASE_LOG_LEVEL:-`grep logLevel ` +  ${BASEDIR} + `/conf/configuration.yml | cut -d ':' -f 2`}
 
-CELLBASE_LOG_FILE="cellbase.json"
-CELLBASE_LOG_LEVEL="info"
+CELLBASE_LOG_LEVEL="INFO"
+CELLBASE_LOG_FILE="log4j2.xml"
 
 if [ $PRG = "cellbase-admin.sh" ]; then
     JAVA_HEAP="8192m"
+    CELLBASE_LOG_FILE="log4j2-json.xml"
 fi
 
 #Set log4j properties file
-export JAVA_OPTS="${JAVA_OPTS} -Dlog4j.configurationFile=file:${BASEDIR}/conf/${LOG_CONFIG}"
-export JAVA_OPTS="${JAVA_OPTS} -Dcellbase.log.file=${CELLBASE_LOG_FILE} -Dcellbase.log.level=${CELLBASE_LOG_LEVEL}"
+export JAVA_OPTS="${JAVA_OPTS} -Dlog4j.configurationFile=file:${BASEDIR}/conf/${CELLBASE_LOG_FILE} "
+export JAVA_OPTS="${JAVA_OPTS} -Dcellbase.log.level=${CELLBASE_LOG_LEVEL} "
 export JAVA_OPTS="${JAVA_OPTS} ${MONITOR_AGENT}"
 export JAVA_OPTS="${JAVA_OPTS} -Dfile.encoding=UTF-8"
 export JAVA_OPTS="${JAVA_OPTS} -Xms256m -Xmx${JAVA_HEAP}"
