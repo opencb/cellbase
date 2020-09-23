281b4bda1c1d41182ecfad90f4e7538e41f3a887
julie-sullivan <julie.sullivan@gmail.com>
2020-09-23 17:01:36 +0100

    remove whitespace

--- cellbase-app/app/conf/cellbase-env.sh
+++ cellbase-app/app/conf/cellbase-env.sh
@@ -7,14 +7,13 @@
 # CLASSPATH_PREFIX
 
 MONITOR_AGENT=""
-if [ -e "monitor/dd-java-agent.jar" ]; then
-    MONITOR_AGENT="-javaagent monitor/dd-java-agent.jar"
+if [ -e "${BASEDIR}/monitor/dd-java-agent.jar" ]; then
+    MONITOR_AGENT="-javaagent:${BASEDIR}/monitor/dd-java-agent.jar"
 fi
 
 JAVA_HEAP="2048m"
-#CELLBASE_LOG_LEVEL=${CELLBASE_LOG_LEVEL:-`grep logLevel ` +  ${BASEDIR} + `/conf/configuration.yml | cut -d ':' -f 2`}
-
-CELLBASE_LOG_LEVEL="INFO"
+CELLBASE_LOG_LEVEL=${CELLBASE_LOG_LEVEL:-`grep logLevel ${BASEDIR}/conf/configuration.yml | cut -d ':' -f 2`}
+CELLBASE_LOG_LEVEL="$(echo "${CELLBASE_LOG_LEVEL}" | tr -d '[:space:]')"
 CELLBASE_LOG_FILE="log4j2.xml"
 
 if [ `basename $PRG` = "cellbase-admin.sh" ]; then
@@ -23,8 +22,8 @@ if [ `basename $PRG` = "cellbase-admin.sh" ]; then
 fi
 
 #Set log4j properties file
-export JAVA_OPTS="${JAVA_OPTS} -Dlog4j.configurationFile=file:${BASEDIR}/conf/${CELLBASE_LOG_FILE} "
-export JAVA_OPTS="${JAVA_OPTS} -Dcellbase.log.level=${CELLBASE_LOG_LEVEL} "
+export JAVA_OPTS="${JAVA_OPTS} -Dlog4j.configurationFile=file:${BASEDIR}/conf/${CELLBASE_LOG_FILE}"
+export JAVA_OPTS="${JAVA_OPTS} -Dcellbase.log.level=${CELLBASE_LOG_LEVEL}"
 export JAVA_OPTS="${JAVA_OPTS} ${MONITOR_AGENT}"
 export JAVA_OPTS="${JAVA_OPTS} -Dfile.encoding=UTF-8"
 export JAVA_OPTS="${JAVA_OPTS} -Xms256m -Xmx${JAVA_HEAP}"
