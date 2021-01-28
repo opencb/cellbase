5b1a7014dfdc8f116b654bfb9daff3e229eea6d8
julie-sullivan <julie.sullivan@gmail.com>
2021-01-28 09:16:11 +0000

    update log files

--- cellbase-app/app/conf/cellbase-env.sh
+++ cellbase-app/app/conf/cellbase-env.sh
@@ -11,17 +11,17 @@ if [ -e "${BASEDIR}/monitor/dd-java-agent.jar" ]; then
     MONITOR_AGENT="-javaagent:${BASEDIR}/monitor/dd-java-agent.jar"
 fi
 
-JAVA_HEAP="16384m"
-CELLBASE_LOG_DIR=${CELLBASE_LOG_DIR}
-CELLBASE_LOG_LEVEL=${CELLBASE_LOG_LEVEL:-"INFO"}
-CELLBASE_LOG_CONFIG="log4j2.file.xml"
+export JAVA_HEAP="100G"
+export CELLBASE_LOG_DIR=${CELLBASE_LOG_DIR}
+export CELLBASE_LOG_LEVEL=${CELLBASE_LOG_LEVEL:-"INFO"}
+export CELLBASE_LOG_CONFIG="log4j2.file.xml"
 
 #Set log4j properties file
-export JAVA_OPTS="${JAVA_OPTS} -Dlog4j.configurationFile=${BASEDIR}/${CELLBASE_LOG_CONFIG}"
+export JAVA_OPTS="${JAVA_OPTS} -Dlog4j2.configurationFile=${BASEDIR}/${CELLBASE_LOG_CONFIG}"
 export JAVA_OPTS="${JAVA_OPTS} -Dcellbase.log.level=${CELLBASE_LOG_LEVEL}"
-export JAVA_OPTS="${JAVA_OPTS} ${MONITOR_AGENT}"
 export JAVA_OPTS="${JAVA_OPTS} -Dfile.encoding=UTF-8"
 export JAVA_OPTS="${JAVA_OPTS} -Xms256m -Xmx${JAVA_HEAP}"
+export JAVA_OPTS="${JAVA_OPTS} ${MONITOR_AGENT}"
 
 if [ -n "$CELLBASE_LOG_DIR" ]; then
     export JAVA_OPTS="${JAVA_OPTS} -Dcellbase.log.dir=${CELLBASE_LOG_DIR}"
