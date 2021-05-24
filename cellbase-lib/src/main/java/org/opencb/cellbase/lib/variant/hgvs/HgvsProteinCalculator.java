6973ccc9028787eeb00560a6934fc83cbf024b72
julie-sullivan <julie.sullivan@gmail.com>
2021-05-24 10:43:25 +0100

    HGVS - include context sequence for insertions to enable prediction of AAs

--- cellbase-lib/src/main/java/org/opencb/cellbase/lib/variant/hgvs/HgvsProteinCalculator.java
+++ cellbase-lib/src/main/java/org/opencb/cellbase/lib/variant/hgvs/HgvsProteinCalculator.java
@@ -296,10 +296,17 @@ public class HgvsProteinCalculator {
 
                 // Get the reference codon and the new sequence inserted
                 String refCodon = transcriptUtils.getCodon(codonPosition);
+                String afterRefCodon = transcriptUtils.getCodon(codonPosition);
                 String refAa = VariantAnnotationUtils.getAminoacid(MT.equals(variant.getChromosome()), refCodon);
 
                 // Build the new inserted sequence = split codon + alternate allele
                 String insSequence = refCodon.substring(0, positionAtCodon - 1) + alternate + refCodon.substring(positionAtCodon - 1);
+
+                // need inserted sequence to be divisible by 3 to predict AAs later
+                if (insSequence.length() % 3 > 0) {
+                    // append NTs until sequence has enough codons
+                    insSequence = insSequence + afterRefCodon.substring(0, (3 - insSequence.length() % 3));
+                }
 
                 // Insertion or Duplication need the reference codon to be the same.
                 // Check if the reference codon is at any end of the new inserted sequence.
