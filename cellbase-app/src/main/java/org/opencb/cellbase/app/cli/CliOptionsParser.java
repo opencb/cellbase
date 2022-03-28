07a4114f1a826fc34c0acc15f797c8218f82dcb2
imedina <igmecas@gmail.com>
2022-03-28 04:00:17 +0100

    app: fix jcommander compilation error

--- cellbase-app/src/main/java/org/opencb/cellbase/app/cli/CliOptionsParser.java
+++ cellbase-app/src/main/java/org/opencb/cellbase/app/cli/CliOptionsParser.java
@@ -145,8 +145,7 @@ public abstract class CliOptionsParser {
 
     protected void printMainUsage() {
         for (String s : jCommander.getCommands().keySet()) {
-//            System.err.printf("%20s  %s\n", s, jCommander.getCommandDescription(s));
-            System.err.printf("%20s  %s\n", s, jCommander.getDescriptions().get(s));
+            System.err.printf("%20s  %s\n", s, jCommander.getCommandDescription(s));
         }
     }
 
@@ -177,8 +176,7 @@ public abstract class CliOptionsParser {
         pad = Math.max(14, pad);
         pad = Math.min(40, pad);
         for (Map.Entry<String, JCommander> entry : commander.getCommands().entrySet()) {
-//            System.err.printf("%" + pad + "s  %s\n", entry.getKey(), commander.getCommandDescription(entry.getKey()));
-            System.err.printf("%" + pad + "s  %s\n", entry.getKey(), commander.getDescriptions().get(entry.getKey()));
+            System.err.printf("%" + pad + "s  %s\n", entry.getKey(), commander.getCommandDescription(entry.getKey()));
         }
     }
 
