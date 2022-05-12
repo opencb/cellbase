9582341b3bf92e88ccaa4688f5c29569566ad879
julie-sullivan <julie.sullivan@gmail.com>
2022-05-12 10:55:09 +0100

    bump library versions

--- cellbase-lib/src/test/java/org/opencb/cellbase/lib/impl/VariantAnnotationCalculatorTest.java
+++ cellbase-lib/src/test/java/org/opencb/cellbase/lib/impl/VariantAnnotationCalculatorTest.java
@@ -1966,27 +1966,6 @@ public class VariantAnnotationCalculatorTest extends GenericMongoDBAdaptorTest {
     }
 
     @Test
-    public void testMissingClinVar() throws Exception {
-
-        Variant variant1 = new Variant("19:11216263:A:AGATTGTAAAGATAAGAGCGAT");
-        //Variant variant1 = new Variant("19:11216245:C:CCGACTGCAAGGACAAATCTGA");
-
-        QueryOptions queryOptions = new QueryOptions("useCache", false);
-        queryOptions.put("include", "hgvs");
-        queryOptions.put("normalize", true);
-        queryOptions.put("skipDecompose", false);
-        queryOptions.put("checkAminoAcidChange", true);
-        queryOptions.put("imprecise", true);
-        queryOptions.put("phased", false);
-
-        QueryResult<VariantAnnotation> queryResult = variantAnnotationCalculator.getAnnotationByVariant(variant1, queryOptions);
-
-        List<String> hgvs = queryResult.getResult().get(0).getHgvs();
-        assertEquals("test", hgvs);
-
-    }
-
-    @Test
     public void testClinicalVariantsGrch38() throws Exception {
 
         initGrch38();
