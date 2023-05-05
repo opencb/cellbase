42c991188ad70157bd5c9f0769b1bd999a745679
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2023-05-05 22:57:00 +0200

    lib: parse PharmGKB clinical annotation files, #TASK-4325. #TASK-1111

--- cellbase-lib/src/main/java/org/opencb/cellbase/lib/builders/PharmGKBBuilder.java
+++ cellbase-lib/src/main/java/org/opencb/cellbase/lib/builders/PharmGKBBuilder.java
@@ -17,7 +17,10 @@
 package org.opencb.cellbase.lib.builders;
 
 import org.apache.commons.lang3.StringUtils;
-import org.opencb.biodata.models.core.PharmaChemical;
+import org.opencb.biodata.models.pharma.PharmaChemical;
+import org.opencb.biodata.models.pharma.PharmaClinicalAllele;
+import org.opencb.biodata.models.pharma.PharmaClinicalAnnotation;
+import org.opencb.biodata.models.pharma.PharmaClinicalEvidence;
 import org.opencb.cellbase.core.exception.CellBaseException;
 import org.opencb.cellbase.core.serializer.CellBaseFileSerializer;
 import org.opencb.commons.exec.Command;
@@ -27,9 +30,7 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.opencb.cellbase.lib.EtlCommons.PHARMGKB_DATA;
@@ -40,7 +41,14 @@ public class PharmGKBBuilder extends CellBaseBuilder {
     private Path inputDir;
     private CellBaseFileSerializer fileSerializer;
 
-    private static final String CHEMICALS_FILENAME = "chemicals";
+    private static final String CHEMICALS_BASENAME = "chemicals";
+    private static final String CHEMICALS_ZIP_FILENAME = "chemicals.zip";
+    private static final String CHEMICALS_TSV_FILENAME = "chemicals.tsv";
+    private static final String CLINICAL_ANNOTATIONS_BASENAME = "clinicalAnnotations";
+    private static final String CLINICAL_ANNOTATIONS_ZIP_FILENAME = "clinicalAnnotations.zip";
+    private static final String CLINICAL_ANNOTATIONS_TSV_FILENAME = "clinical_annotations.tsv";
+    private static final String CLINICAL_ANN_ALLELES_TSV_FILENAME = "clinical_ann_alleles.tsv";
+    private static final String CLINICAL_ANN_EVIDENCE_TSV_FILENAME = "clinical_ann_evidence.tsv";
 
     public PharmGKBBuilder(Path inputDir, CellBaseFileSerializer serializer) {
         super(serializer);
@@ -62,16 +70,144 @@ public class PharmGKBBuilder extends CellBaseBuilder {
         Path inPharmGKBDir = inputDir.resolve(PHARMGKB_DATA);
         FileUtils.checkPath(inPharmGKBDir);
 
-        unzip(inPharmGKBDir, CHEMICALS_FILENAME + ".zip", inPharmGKBDir.resolve(CHEMICALS_FILENAME));
+        // Chemicals
+        unzip(inPharmGKBDir, CHEMICALS_ZIP_FILENAME, Collections.singletonList(CHEMICALS_TSV_FILENAME),
+                inPharmGKBDir.resolve(CHEMICALS_BASENAME));
+        Map<String, PharmaChemical> chemicalsMap = parseChemicals(inPharmGKBDir.resolve(CHEMICALS_BASENAME)
+                .resolve(CHEMICALS_TSV_FILENAME));
+        logger.info("Chemical mapsize = " + chemicalsMap.size());
 
-        Map<String, PharmaChemical> chemicalsMap = parseChemicals(inPharmGKBDir.resolve(CHEMICALS_FILENAME)
-                .resolve(CHEMICALS_FILENAME + ".tsv"));
+        // Clinical annotation
+        unzip(inPharmGKBDir, CLINICAL_ANNOTATIONS_ZIP_FILENAME, Arrays.asList(CLINICAL_ANNOTATIONS_TSV_FILENAME,
+                        CLINICAL_ANN_ALLELES_TSV_FILENAME, CLINICAL_ANN_EVIDENCE_TSV_FILENAME),
+                inPharmGKBDir.resolve(CLINICAL_ANNOTATIONS_BASENAME));
+        parseClinicalAnnotations(inPharmGKBDir.resolve(CLINICAL_ANNOTATIONS_BASENAME), chemicalsMap);
+
         for (Map.Entry<String, PharmaChemical> entry : chemicalsMap.entrySet()) {
             logger.info(entry.getKey() + " -> " + entry.getValue());
         }
-        logger.info("Chemical mapsize = " + chemicalsMap.size());
 
         logger.info("Parsing PharmGKB files finished.");
+    }
+
+    private void parseClinicalAnnotations(Path annPath, Map<String, PharmaChemical> chemicalsMap) throws IOException {
+        Map<String, PharmaClinicalAnnotation> annotationMap = new HashMap<>();
+        Map<String, List<String>> drugToAnnotationIdMap = new HashMap<>();
+
+        // clinical_annotations.tsv
+        try (BufferedReader br = FileUtils.newBufferedReader(annPath.resolve(CLINICAL_ANNOTATIONS_TSV_FILENAME))) {
+            // Skip first line, i.e. the header line
+            String line = br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split("\t", -1);
+                // 0                       1                   2     3                  4               5                6
+                // Clinical Annotation ID  Variant/Haplotypes  Gene  Level of Evidence  Level Override  Level Modifiers  Score
+                // 7                   8           9               10       11            12                                13
+                // Phenotype Category  PMID Count  Evidence Count  Drug(s)  Phenotype(s)  Latest History Date (YYYY-MM-DD)  URL
+                // 14
+                // Specialty Population
+                PharmaClinicalAnnotation clinicalAnnotation = new PharmaClinicalAnnotation()
+                        .setVariantId(fields[1])
+                        .setGene(fields[2])
+                        .setLevelOfEvidence(fields[3])
+                        .setLevelOverride(fields[4])
+                        .setLevelModifiers(fields[5])
+                        .setScore(fields[6])
+                        .setPhenotypeCategory(fields[7])
+                        .setLatestUpdateDate(fields[12])
+                        .setUrl(fields[13])
+                        .setSpecialtyPopulation(fields[14]);
+
+                if (StringUtils.isNotEmpty(fields[11])) {
+                    clinicalAnnotation.setPhenotypes(stringFieldToList(fields[11]));
+                }
+
+                // Sanity check
+                if (StringUtils.isEmpty(fields[0])) {
+                    logger.warn("Clinical annotation ID is missing in clinical annotations line: {}", line);
+                    continue;
+                }
+                // Add the annotation to the annotationMap by annotation ID
+                annotationMap.put(fields[0], clinicalAnnotation);
+
+                // Process the drug names to update the drugToAnnotationId map
+                if (StringUtils.isNotEmpty(fields[10])) {
+                    // Drugs are separated by ;
+                    String[] drugs = fields[10].split(";");
+                    for (String drug : drugs) {
+                        if (!drugToAnnotationIdMap.containsKey(drug)) {
+                            // Add the drug to the map
+                            drugToAnnotationIdMap.put(drug, new ArrayList<>());
+                        }
+                        // Add the annotation ID to that drug
+                        drugToAnnotationIdMap.get(drug).add(fields[0]);
+                    }
+                }
+            }
+        }
+
+        // clinical_ann_evidence.tsv
+        try (BufferedReader br = FileUtils.newBufferedReader(annPath.resolve(CLINICAL_ANN_EVIDENCE_TSV_FILENAME))) {
+            // Skip first line, i.e. the header line
+            String line = br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split("\t", -1);
+                // 0                       1            2              3             4     5        6
+                // Clinical Annotation ID  Evidence ID  Evidence Type  Evidence URL  PMID  Summary  Score
+                PharmaClinicalEvidence evidence = new PharmaClinicalEvidence()
+                        .setType(fields[2])
+                        .setUrl(fields[3])
+                        .setPmid(fields[4])
+                        .setSummary(fields[5])
+                        .setScore(fields[6]);
+
+                // Sanity check
+                if (StringUtils.isEmpty(fields[0])) {
+                    logger.warn("Clinical annotation ID is missing in clinical annotation evidence line: {}", line);
+                    continue;
+                }
+                if (annotationMap.containsKey(fields[0])) {
+                    annotationMap.get(fields[0]).getEvidences().add(evidence);
+                } else {
+                    logger.warn("Clinical annotation ID {} from clinical annotation evidence not found in clinical annotations", fields[0]);
+                }
+            }
+        }
+
+        // clinical_ann_alleles.tsv
+        try (BufferedReader br = FileUtils.newBufferedReader(annPath.resolve(CLINICAL_ANN_ALLELES_TSV_FILENAME))) {
+            // Skip first line, i.e. the header line
+            String line = br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split("\t", -1);
+                // 0                       1                2                3
+                // Clinical Annotation ID  Genotype/Allele  Annotation Text  Allele Function
+                PharmaClinicalAllele clinicalAllele = new PharmaClinicalAllele()
+                        .setAllele(fields[1])
+                        .setAnnotation(fields[2])
+                        .setFunction(fields[3]);
+
+                // Sanity check
+                if (StringUtils.isEmpty(fields[0])) {
+                    logger.warn("Clinical annotation ID is missing in clinical annotation alleles line: {}", line);
+                    continue;
+                }
+                if (annotationMap.containsKey(fields[0])) {
+                    annotationMap.get(fields[0]).getAlleles().add(clinicalAllele);
+                } else {
+                    logger.warn("Clinical annotation ID {} from clinical annotation allele not found in clinical annotations", fields[0]);
+                }
+            }
+        }
+
+        // Update chemicals map by adding the clinical annotation
+        for (String drug : drugToAnnotationIdMap.keySet()) {
+            if (chemicalsMap.containsKey(drug)) {
+                for (String annotationId : drugToAnnotationIdMap.get(drug)) {
+                    chemicalsMap.get(drug).getAnnotations().add(annotationMap.get(annotationId));
+                }
+            }
+        }
     }
 
     private Map<String, PharmaChemical> parseChemicals(Path chemicalsFile) throws IOException {
@@ -80,7 +216,7 @@ public class PharmGKBBuilder extends CellBaseBuilder {
             // Skip first line, i.e. the header line
             String line = br.readLine();
             while ((line = br.readLine()) != null) {
-                String[] fields = line.split("\t");
+                String[] fields = line.split("\t", -1);
                 // 0                      1     2              3            4              5    6                7      8
                 // PharmGKB Accession Id  Name  Generic Names  Trade Names  Brand Mixtures Type Cross-references SMILES InChI
                 // 9                10                  11                        12                       13            14
@@ -92,26 +228,61 @@ public class PharmGKBBuilder extends CellBaseBuilder {
                 PharmaChemical pharmaChemical = new PharmaChemical()
                         .setId(fields[0])
                         .setSource(PHARMGKB_NAME)
-                        .setName(fields[1]);
+                        .setName(fields[1])
+                        .setSmiles(fields[7])
+                        .setInChI(fields[8]);
 
+                // Generic Names
+                if (StringUtils.isNotEmpty(fields[2])) {
+                    pharmaChemical.setGenericNames(stringFieldToList(fields[2]));
+                }
+
+                // Trade Names
+                if (StringUtils.isNotEmpty(fields[3])) {
+                    pharmaChemical.setTradeNames(stringFieldToList(fields[3]));
+                }
+
+                // Brand Mixtures
+                if (StringUtils.isNotEmpty(fields[4])) {
+                    pharmaChemical.setTradeMixtures(stringFieldToList(fields[4]));
+                }
+
+                // Types
                 if (StringUtils.isNotEmpty(fields[5])) {
                     pharmaChemical.setTypes(Arrays.stream(fields[5].split(",")).map(s -> s.trim()).collect(Collectors.toList()));
                 }
 
-                chemicalsMap.put(pharmaChemical.getId(), pharmaChemical);
+                chemicalsMap.put(pharmaChemical.getName(), pharmaChemical);
             }
         }
         return chemicalsMap;
     }
 
-    private void unzip(Path inPath, String zipFilename, Path outPath) throws CellBaseException {
+    private List<String> stringFieldToList(String field) {
+        if (field.startsWith("\"")) {
+            // FIXME: double double quotes
+            return Arrays.stream(field.replace("\", \"", "\",\"").split("\",\"")).map(s -> s.trim()).collect(Collectors.toList());
+        } else if (field.contains(", ")) {
+            return Arrays.stream(field.replace(", ", ",").split(",")).map(s -> s.trim()).collect(Collectors.toList());
+        } else {
+            return Collections.singletonList(field);
+        }
+    }
+
+    private void unzip(Path inPath, String zipFilename, List<String> outFilenames, Path outPath) throws CellBaseException {
         if (!outPath.toFile().exists()) {
             if (!inPath.resolve(zipFilename).toFile().exists()) {
-                throw new CellBaseException("PharmGKB file '" + zipFilename + ".zip found");
+                throw new CellBaseException("PharmGKB file '" + zipFilename + "' not found");
             }
-            logger.info("Uncompressing {} into {}", zipFilename, outPath);
+            logger.info("Unzipping {} into {}", zipFilename, outPath);
             Command cmd = new Command("unzip -d " + outPath + " " + inPath.resolve(zipFilename));
             cmd.run();
+
+            for (String outFilename : outFilenames) {
+                if (!outPath.resolve(outFilename).toFile().exists()) {
+                    throw new CellBaseException("After unzipping the PharmGKB file '" + outFilename + "' not found");
+                }
+            }
         }
     }
 }
