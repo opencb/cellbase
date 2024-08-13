f86423c0e7d3fcd83dc13971fbdc61cc4a0ad4b6
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2024-08-13 14:11:58 +0200

    lib: fix checkstyle issue, #TASK-6565

--- cellbase-lib/src/main/java/org/opencb/cellbase/lib/impl/core/singleton/DataReleaseSingleton.java
+++ cellbase-lib/src/main/java/org/opencb/cellbase/lib/impl/core/singleton/DataReleaseSingleton.java
@@ -35,7 +35,6 @@ import org.opencb.commons.datastore.mongodb.MongoDBCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
