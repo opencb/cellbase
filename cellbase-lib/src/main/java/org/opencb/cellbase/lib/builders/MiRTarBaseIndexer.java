2a0eb2814c7f135b73db406194cdfd26b02d8fbf
Joaquín Tárraga Giménez <joaquintarraga@gmail.com>
2025-02-20 12:29:44 +0100

    lib: update MiRTarBase indexer to take into account the new format (CSV file), #TASK-5564

--- cellbase-lib/src/main/java/org/opencb/cellbase/lib/builders/MiRTarBaseIndexer.java
+++ cellbase-lib/src/main/java/org/opencb/cellbase/lib/builders/MiRTarBaseIndexer.java
@@ -25,6 +25,7 @@ import org.opencb.commons.utils.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -54,93 +55,74 @@ public class MiRTarBaseIndexer {
 
         Map<String, List<MirnaTarget>> geneToMirna = new HashMap<>();
 
-        try (InputStream fis = new FileInputStream(miRTarBaseFile.toFile());
-             Workbook workbook = new XSSFWorkbook(fis)) {
+//        String currentMiRTarBaseId = null;
+//        String currentMiRNA = null;
+//        String currentGene = null;
+        List<TargetGene> targetGenes = new ArrayList<>();
 
-            // Get the first sheet
-            Sheet sheet = workbook.getSheetAt(0);
-
-            String currentMiRTarBaseId = null;
-            String currentMiRNA = null;
-            String currentGene = null;
-            List<TargetGene> targetGenes = new ArrayList<>();
-
-            for (int rowNum = sheet.getFirstRowNum() + 1; rowNum <= sheet.getLastRowNum(); rowNum++) {
-                Row row = sheet.getRow(rowNum);
-
-                // Sanity check
-                if (row.getPhysicalNumberOfCells() != 9) {
-                    logger.warn("Error parsing line {}: invalid number of columns {} (expected 9 columns). Line {}.",
-                            rowNum + 1, row.getPhysicalNumberOfCells());
-                    continue;
-                }
-
-                if (row.getCell(0).getCellType() != CellType.STRING || row.getCell(0).getStringCellValue() == null
-                        || row.getCell(1).getCellType() != CellType.STRING || row.getCell(1).getStringCellValue() == null
-                        || row.getCell(3).getCellType() != CellType.STRING || row.getCell(3).getStringCellValue() == null) {
-                    logger.warn("Error parsing line {}: mandatory fields(miRTarBase ID, miRNA, Target Gene) are empty or wrong cell type.",
-                            rowNum + 1);
-                    continue;
-                }
+        try (BufferedReader br = FileUtils.newBufferedReader(miRTarBaseFile)) {
+            // Skip first line, i.e. the header line
+            String line = br.readLine();
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",", -1);
 
                 // #0: miRTarBase ID
-                Cell cell = row.getCell(0);
-                String miRTarBaseId = cell.getStringCellValue();
-                if (currentMiRTarBaseId == null) {
-                    currentMiRTarBaseId = miRTarBaseId;
-                }
+                String miRTarBaseId = fields[0];
 
                 // #1: miRNA
-                cell = row.getCell(1);
-                String miRNA = cell.getStringCellValue();
-                if (currentMiRNA == null) {
-                    currentMiRNA = miRNA;
-                }
+                String miRNA = fields[1];
 
                 // #2: Species (miRNA)
 
                 // #3: Target Gene
-                cell = row.getCell(3);
-                String geneName = cell.getStringCellValue();
-                if (currentGene == null) {
-                    currentGene = geneName;
-                }
+                String geneName = fields[3];
 
                 // #4: Target Gene (Entrez ID)
                 // #5: Species (Target Gene)
 
-                if (!miRTarBaseId.equals(currentMiRTarBaseId) || !geneName.equals(currentGene)) {
-                    // new entry, store current one
-                    MirnaTarget miRnaTarget = new MirnaTarget(currentMiRTarBaseId, MIRTARBASE_DATA, currentMiRNA, targetGenes);
-                    GeneBuilderIndexer.addValueToMapElement(geneToMirna, currentGene, miRnaTarget);
-                    targetGenes = new ArrayList<>();
-                    currentGene = geneName;
-                    currentMiRTarBaseId = miRTarBaseId;
-                    currentMiRNA = miRNA;
-                }
-
                 // #6: Experiments
-                cell = row.getCell(6);
-                String experiment = (cell.getCellType() == CellType.STRING ? cell.getStringCellValue() : null);
+                String experiment = fields[6];
 
                 // #7: Support Type
-                cell = row.getCell(7);
-                String supportType = (cell.getCellType() == CellType.STRING ? cell.getStringCellValue() : null);
+                String supportType = fields[7];
 
                 // #8: pubmed
-                cell = row.getCell(8);
-                String pubmed = new BigDecimal(cell.getNumericCellValue()).toString();
+                String pubmed = fields[8];
+                if (StringUtils.isNotEmpty(pubmed) && pubmed.contains(".")) {
+                    pubmed = pubmed.split("\\.")[0];
+                }
 
+                // Add to map
+                if (!geneToMirna.containsKey(geneName)) {
+                    geneToMirna.put(geneName, new ArrayList<>());
+                }
+                List<MirnaTarget> mirnaTargets = geneToMirna.get(geneName);
+                MirnaTarget target = null;
+                for (MirnaTarget mirnaTarget : mirnaTargets) {
+                   if (mirnaTarget.getId().equals(miRTarBaseId) && mirnaTarget.getSourceId().equals(miRNA)) {
+                       target = mirnaTarget;
+                       break;
+                   }
+                }
+                if (target == null) {
+                    target = new MirnaTarget(miRTarBaseId, MIRTARBASE_DATA, miRNA, new ArrayList<>());
+                    geneToMirna.get(geneName).add(target);
+                }
                 if (StringUtils.isNotEmpty(experiment) || StringUtils.isNotEmpty(supportType) || StringUtils.isNotEmpty(pubmed)) {
-                    targetGenes.add(new TargetGene(experiment, supportType, pubmed));
+                    boolean found = false;
+                    for (TargetGene targetGene : target.getTargets()) {
+                        if (targetGene.getExperiment().equals(experiment) && targetGene.getEvidence().equals(supportType) && targetGene.getPubmed().equals(pubmed)) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        target.getTargets().add(new TargetGene(experiment, supportType, pubmed));
+                    }
                 }
             }
-
-            // parse last entry
-            MirnaTarget miRnaTarget = new MirnaTarget(currentMiRTarBaseId, MIRTARBASE_DATA, currentMiRNA, targetGenes);
-            GeneBuilderIndexer.addValueToMapElement(geneToMirna, currentGene, miRnaTarget);
-
         }
+
         logger.info(PARSING_DONE_LOG_MESSAGE, miRTarBaseFile);
 
         return geneToMirna;
